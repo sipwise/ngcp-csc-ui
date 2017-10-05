c240ba319ffb28db6f56ce12d7b85692991426c4
Hans-Peter Herzog <hherzog@sipwise.com>
2017-10-05 20:38:52 +0200

    TT#23018 Setup: Create and prepare project skeleton for new CSC Web UI
    - Initial vue + vuex + vue-router + webpack + quasar setup
    - Added karma test runner
    - Added shrinkwrap file
    - Added junit reporter
    - Fetch subscriber
    - Fetch capabilities
    - Added Dockerfile + testrunner
    - Added development script for ngcp
    
    Change-Id: Ia9e58412d2b71c7e541f3d764f8fda747522c7e6

---
+++ src/store/index.js
@@ -0,0 +1,105 @@
+'use strict';
+
+import _ from 'lodash'
+import Vue from 'vue'
+import Vuex from 'vuex'
+// import cdk from 'cdk';
+
+var rtcEngineClient = null;
+var rtcEngineNetwork = null;
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+    modules: {
+        user: require('./user').UserModule
+    },
+    state: {
+        rtcEngineConnected: false
+    },
+    getters: {},
+    mutations: {
+        disconnectRtcEngine(state) {
+            state.rtcEngineConnected = false;
+        },
+        connectRtcEngine(state) {
+            state.rtcEngineConnected = true;
+        }
+    },
+    actions: {
+        createRtcEngineSession(context) {
+            return new Promise((resolve, reject)=>{
+                Promise.resolve().then(()=>{
+                    return Vue.http.post('/api/rtcsessions/');
+                }).then((res)=>{
+                    return Vue.http.get(res.headers.get('Location'));
+                }).then((res)=>{
+                    return res.json();
+                }).then((body)=>{
+                    localStorage.setItem('rtcEngineSession', body.rtc_browser_token);
+                    resolve(localStorage.getItem('rtcEngineSession'));
+                }).catch((err)=>{
+                    reject(err);
+                });
+            });
+        },
+        connectRtcEngine(context, options) {
+            return new Promise((resolve, reject)=>{
+
+                var force = _.get(options, 'force', false);
+                var isConnected = rtcEngineClient instanceof cdk.Client && _.isEmpty(rtcEngineClient.disconnectReason);
+
+                if(isConnected && !force) {
+                    resolve();
+                } else {
+                    Promise.resolve().then(()=>{
+                        return context.dispatch('disconnectRtcEngine');
+                    }).then(()=>{
+                        return context.dispatch('createRtcEngineSession');
+                    }).then((sessionToken)=>{
+                        rtcEngineClient = new cdk.Client({
+                            url: 'wss://' + window.location.host + '/rtc/api',
+                            userSession: sessionToken
+                        });
+                        rtcEngineClient.onConnect(()=>{
+                            rtcEngineNetwork = rtcEngineClient.getNetworkByTag('sip');
+                            rtcEngineNetwork.onConnect(()=>{
+                                context.commit('connectRtcEngine');
+                                resolve();
+                            });
+                            rtcEngineNetwork.onDisconnect(()=>{
+                                context.commit('disconnectRtcEngine');
+                                reject(new Error('NetworkError: ' + rtcEngineNetwork.disconnectReason));
+                            });
+                        });
+                        rtcEngineClient.onDisconnect(()=>{
+                            context.commit('disconnectRtcEngine');
+                            reject(new Error('ClientError: ' + rtcEngineClient.disconnectReason));
+                        });
+                    }).catch((err)=>{
+                        context.commit('disconnectRtcEngine');
+                        reject(err);
+                    });
+                }
+            });
+        },
+        disconnectRtcEngine(context) {
+            return new Promise((resolve, reject)=>{
+                context.commit('disconnectRtcEngine');
+                localStorage.removeItem('rtcEngineSession');
+                if(rtcEngineClient instanceof cdk.Client && _.isEmpty(rtcEngineClient.disconnectReason)) {
+                    rtcEngineClient.onDisconnect(()=>{
+                        rtcEngineClient = null;
+                        rtcEngineNetwork = null;
+                        resolve();
+                    });
+                    rtcEngineClient.disconnect();
+                } else {
+                    rtcEngineClient = null;
+                    rtcEngineNetwork = null;
+                    resolve();
+                }
+            });
+        }
+    }
+});
