3909f72fb891a94fb9df3b6e9b9cd170ed0e75ee
Hans-Peter Herzog <hherzog@sipwise.com>
2017-10-05 20:38:52 +0200

    TT#23018 Setup: Create and prepare project skeleton for new CSC Web UI
    - Initial vue + vuex + vue-router + webpack + quasar setup
    - Added karma test runner
    - Added shrinkwrap file
    - Added junit reporter
    - Fetch subscriber
    - Fetch capabilities
    - Added Dockerfile + testrunner
    - Added development script for ngcp
    
    Change-Id: I67332f9e079316057deb7326ad3017abbf7b807d

---
+++ src/components/pages/CallBlocking/Outgoing.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>CallBlocking Outgoing</div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {}
+        }
+    }
+</script>
+
+<style>
+</style>
