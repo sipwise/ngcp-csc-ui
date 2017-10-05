e52c083b438aae58ca1d78ad589f764ac4de5a44
Hans-Peter Herzog <hherzog@sipwise.com>
2017-10-05 20:38:52 +0200

    TT#23018 Setup: Create and prepare project skeleton for new CSC Web UI
    - Initial vue + vuex + vue-router + webpack + quasar setup
    - Added karma test runner
    - Added shrinkwrap file
    - Added junit reporter
    - Fetch subscriber
    - Fetch capabilities
    - Added Dockerfile + testrunner
    - Added development script for ngcp
    
    Change-Id: Ia9e58412d2b71c7e541f3d764f8fda747522c7e6

---
+++ src/components/pages/CallForward/CompanyHours.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>CallForward CompanyHours</div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {}
+        }
+    }
+</script>
+
+<style>
+</style>
