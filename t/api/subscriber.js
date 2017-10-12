077393c1dd7e0ffcd88e81dbf6225df08a42cc1e
Hans-Peter Herzog <hherzog@sipwise.com>
2017-10-12 15:20:07 +0200

    TT#23436 Add first api client test as template
    Change-Id: Id58d18d5f9ac2ec16707e35bb7710fbbc914763f

---
+++ t/api/subscriber.js
@@ -0,0 +1,47 @@
+'use strict';
+
+import Vue from 'vue';
+import VueResource from 'vue-resource';
+import { getPreferences } from '../../src/api/subscriber';
+import { assert } from 'chai';
+
+Vue.use(VueResource);
+
+
+describe('Subscriber', function(){
+
+    it('should get all subscriber preferences', function(done) {
+        Vue.http.interceptors = [];
+        Vue.http.interceptors.unshift((request, next)=>{
+            next(request.respondWith(JSON.stringify({
+                block_in_mode: false
+            }), {
+                status: 200
+            }));
+        });
+        getPreferences('123').then((result)=>{
+            assert.property(result, 'block_in_mode');
+            assert.isFalse(result.block_in_mode);
+            done();
+        }).catch((err)=>{
+            done(err);
+        });
+    });
+
+    it('should handle a 403 Forbidden while requesting the preferences', function(done) {
+        Vue.http.interceptors = [];
+        Vue.http.interceptors.unshift((request, next)=>{
+            next(request.respondWith(JSON.stringify({
+                message: '403 Forbidden'
+            }), {
+                status: 403
+            }));
+        });
+        getPreferences('123').then(()=>{
+            done(new Error('Test failed'));
+        }).catch((err)=>{
+            assert.equal(err.status, 403);
+            done();
+        });
+    });
+});
