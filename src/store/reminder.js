c315b720d9bd1f3154639d614561ee0a49546221
Carlo <cvenusino@sipwise.com>
2017-10-16 15:34:48 +0200

    TT#23168 CSC Reminder module
    Change-Id: Idb195edd13b9b1bc75dc130b31d5a6c6e29275bf

---
+++ src/store/reminder.js
@@ -0,0 +1,102 @@
+'use strict';
+
+import _ from 'lodash';
+import {
+    getReminder,
+    createReminder,
+    enableReminder,
+    disableReminder,
+    setTime,
+    setRecurrence
+} from '../api/reminder';
+
+
+export default {
+    namespaced: true,
+    state: {},
+    getters: {},
+    mutations: {
+        enableReminder(state) {
+            state.active = true;
+        },
+        disableReminder(state) {
+            state.active = false;
+        },
+        setTime(state, newTime) {
+            state.time = newTime;
+        },
+        setRecurrence(state, recurrence) {
+            state.recurrence = recurrence;
+        },
+        reminderLoaded(state, options) {
+            state.reminderID = options.id;
+            state.active = options.active;
+            state.time = options.time;
+            state.recurrence = options.recur;
+        },
+        reminderCreated(state, reminderID) {
+            state.reminderID = reminderID;
+        }
+    },
+    actions: {
+        loadReminder(context) {
+            return new Promise((resolve, reject) => {
+                getReminder(localStorage.getItem('subscriberId')).then((result) => {
+                    if (result.total_count > 0) {
+                        context.commit('reminderLoaded', result._embedded['ngcp:reminders'][0]); // open to suggestions on how to extract data here
+                    }else{
+                        // If no default reminder is set, then we need to create it.
+                        createReminder(localStorage.getItem('subscriberId')).then((result) => {
+                            context.commit('reminderCreated', result);
+                            resolve();
+                        }).catch((err) => {
+                            reject(err);
+                        });
+                    }
+                    resolve();
+                }).catch((err) => {
+                    reject(err);
+                });
+            });
+        },
+        toggleReminder(context, enabled) {
+            return new Promise((resolve, reject) => {
+                if(enabled === true){
+                    enableReminder(context.state.reminderID).then((result) => {
+                        context.commit('enableReminder');
+                        resolve();
+                    }).catch((err) => {
+                        reject(err);
+                    });
+                }else{
+                    disableReminder(context.state.reminderID).then((result) => {
+                        context.commit('disableReminder');
+                        resolve();
+                    }).catch((err) => {
+                        reject(err);
+                    });
+                }
+            });
+        },
+        changeTime(context, time) {
+            return new Promise((resolve, reject) => {
+                setTime(context.state.reminderID, time).then((result) => {
+                    context.commit('setTime', time);
+                    resolve();
+                }).catch((err) => {
+                    reject(err);
+                });
+            });
+        },
+        changeRecurrence(context, recurrence) {
+            return new Promise((resolve, reject) => {
+                setRecurrence(context.state.reminderID, recurrence).then((result) => {
+                    context.commit('setRecurrence', recurrence);
+                    resolve();
+                }).catch((err) => {
+                    reject(err);
+                });
+            });
+        }
+    }
+};
