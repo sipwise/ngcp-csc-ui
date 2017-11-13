bd77baa123da8c16a7bf3b29bd0045c08be5f3e9
raxelsen <raxelsen@sipwise.com>
2017-11-13 15:13:04 +0100

    TT#23958 Implement infinite scroll
    Change-Id: Iae794486adfefea685799802ab036ee4c1957542

--- src/api/conversations.js
+++ src/api/conversations.js
@@ -1,11 +1,16 @@
 
 import Vue from 'vue';
+import _ from 'lodash';
 import { getJsonBody } from './utils'
 
-export function getConversations(id) {
+export function getConversations(id, page, rows) {
+    let params = { subscriber_id: id, page: page, rows: rows };
     return new Promise((resolve, reject) => {
-        Vue.http.get('/api/conversations/?subscriber_id=' + id).then((result)=>{
-            resolve(getJsonBody(result.body)._embedded['ngcp:conversations']);
+        Vue.http.get('/api/conversations/', { params: params }).then((result)=>{
+            let jsonBody = getJsonBody(result.body);
+            if (_.has(jsonBody, "_embedded.ngcp:conversations")) {
+                resolve(jsonBody._embedded['ngcp:conversations']);
+            };
         }).catch((err)=>{
             reject(err);
         });
