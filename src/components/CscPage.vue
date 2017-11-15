9dcbd86a7cb316555114f430743e152367d2748d
raxelsen <raxelsen@sipwise.com>
2017-11-15 16:16:04 +0100

    TT#24903 Improve page title styling
    Change-Id: I95c6f4a5ba1b395cf32158145d1262a06e470696

--- src/components/CscPage.vue
+++ src/components/CscPage.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="page">
+		<!--TODO: Toggle class, bind class property (array in the class for example). Use the new introduced sidebar state from hherzog to toggle with-->
         <q-fixed-position corner="top-left" :offset="[0,0]" class="page-title transition-generic">
             <h2>{{ title }}</h2>
         </q-fixed-position>
@@ -32,52 +33,38 @@
 <style lang="stylus">
     @import '../../src/themes/app.variables.styl';
     @import '../../src/themes/quasar.variables.styl';
-    .page {
-        position: relative;
-        padding: 60px;
-        padding-top: 100px;
-    }
-
-    .page h2 {
-        margin: 0px;
-        font-size: 26px;
-        line-height: 26px;
-        color: #68A44E;
-    }
-
-    .page .page-title {
-        padding: 30px;
-        padding-left: 60px;
-        padding-right: 60px;
-        background-color: white;
-        z-index: 1000;
-    }
-
-    .page .page-button {
-        padding-top: 20px;
-        padding-right: 60px;
-    }
-
-    @media (max-width: $breakpoint-sm) {
-        .page {
-            padding: 30px;
-            padding-top: 100px;
-        }
-        .page h2 {
-            margin: 0px;
-            font-size: 22px;
-            line-height: 22px;
-        }
-
-        .page .page-title {
-            padding: 30px;
-            background-color: white;
-            z-index: 1000;
-        }
-    }
-
-    .page-title-icon {
-        margin-right: 10px;
-        font-size: 24px !important;
-    }
+.page
+    position relative
+    padding 60px
+    padding-top 100px
+.page h2
+    margin 0px
+    font-size 26px
+    line-height 26px
+    color #68A44E
+.page .page-title
+    padding 30px
+    padding-left 60px
+    padding-right 60px
+    background linear-gradient(to bottom, rgba(255, 255, 255, 1), rgba(255, 255, 255, 1) 70%, rgba(0, 0, 0, 0))
+    z-index 1000
+    width 100%
+.page .page-button
+    padding-top 20px
+    padding-right 60px
+@media (max-width $breakpoint-sm)
+    .page
+        padding 30px
+        padding-top 100px
+    .page h2
+        margin 0px
+        font-size 22px
+        line-height 22px
+    .page .page-title
+        padding 30px
+        z-index 1000
+    	background linear-gradient(to bottom, rgba(255, 255, 255, 1), rgba(255, 255, 255, 1) 70%, rgba(0, 0, 0, 0))
+.page-title-icon
+    margin-right 10px
+    font-size 24px !important
 </style>
