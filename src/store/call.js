4be8ee40fe6d59c81f7a35d633e97a6478007836
raxelsen <raxelsen@sipwise.com>
2017-11-15 17:34:49 +0100

    TT#24843 Implement call action button
    Change-Id: I433a0ebccd84c8926202d67c9b3284edcf869620

--- src/store/call.js
+++ src/store/call.js
@@ -164,7 +164,11 @@ export default {
          * @param options.number
          */
         start(context, options) {
+            console.log('start()');
+            console.log('options.number is', options.number, 'and options.localMedia is', options.localMedia);
+            context.commit('layout/showRight', null, { root: true });
             Vue.call.createLocalMedia(options.localMedia).then((localMediaStream)=>{
+                console.log('Vue.call.createLocalMediai()');
                 var call = Vue.call.start(options.number, localMediaStream);
                 call.onAccepted(()=>{
                     }).onEnded(()=>{
