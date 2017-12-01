85651ee352cb29d91e5269e3de3d04fe2b35310d
raxelsen <raxelsen@sipwise.com>
2017-12-01 12:31:26 +0100

    TT#20530 Implement playable voicemail
    What has been done:
    - TT#27761, Conversations: Create custom modules for conversation
      items
    - TT#27763, Conversations: Implement "play voicemail" button with
      download functionality
    - TT#27764, Conversations: Implement hashed id based on conversation
      item type, call_type and id
    
    Change-Id: If772b3ed7e7db1dd7b93e48aacf1ce0d93acf5a8

--- src/api/conversations.js
+++ src/api/conversations.js
@@ -1,6 +1,8 @@
 
-import Vue from 'vue';
-import _ from 'lodash';
+import { saveAs } from 'file-saver'
+import Vue from 'vue'
+import _ from 'lodash'
+import crypto from 'crypto-browserify'
 import { getJsonBody } from './utils'
 
 export function getConversations(id, page, rows) {
@@ -11,12 +13,40 @@ export function getConversations(id, page, rows) {
             .then(result => {
                 let jsonBody = getJsonBody(result.body);
                 if (_.has(jsonBody, "_embedded.ngcp:conversations")) {
-                    resolve(jsonBody._embedded['ngcp:conversations']);
+                    let list = [];
+                    _.forEach(jsonBody._embedded['ngcp:conversations'], function(item) {
+                        let inputString = `${item.type}${item.call_type}${item.id}`;
+                        let id = crypto.createHash('sha256').update(inputString).digest('base64');
+                        item._id = id;
+                        if (item._links['ngcp:voicemailrecordings']) {
+                            item.voicemail = item._links['ngcp:voicemailrecordings'].href;
+                        };
+                        delete item._links;
+                        if (item.type == 'call') {
+                            item.type = item.call_type != 'call' ? 'callforward'
+                                : item.type;
+                        };
+                        list.push(item);
+                    });
+                    resolve(list);
                 } else {
                     reject(new Error('No items returned for this page.'))
                 };
             }).catch(err => {
                 reject(err);
             });
+    });
+}
+
+export function downloadVoiceMail(id) {
+    return new Promise((resolve, reject)=>{
+        Vue.http.get('/api/voicemailrecordings/' + id, {}, {
+            responseType: 'arraybuffer'
+        }).then((res)=>{
+            saveAs(new Blob([res.data], { type: res.headers.get('Content-Type') }), "voicemail-" + id + '.wav');
+            resolve();
+        }).catch((err)=>{
+            reject(err);
+        });
     });
 }
