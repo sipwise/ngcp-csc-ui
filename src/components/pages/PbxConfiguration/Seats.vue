6ec5bc96a1f164f35b9f20f152f9dd427e92c4a7
Hans-Peter Herzog <hherzog@sipwise.com>
2017-12-18 09:48:33 +0100

    TT#20509 PBXConfig: As a Customer, I want to see a list of all PBXGroups
    Change-Id: Ib4c4256b07ec177609fccab8ad42f695b7cef5d8

--- src/components/pages/PbxConfiguration/Seats.vue
+++ src/components/pages/PbxConfiguration/Seats.vue
@@ -1,18 +1,52 @@
 <template>
-    <csc-page title="Seats"></csc-page>
+    <csc-page title="Seats">
+        <q-card v-for="seat in seats" class="pbx-seat">
+            <q-card-title>
+                <q-icon name="person" color="primary" size="22px"/>
+                <span class="pbx-seat-title">{{ seat.display_name }}</span>
+                <csc-number-chip :item="seat" />
+            </q-card-title>
+        </q-card>
+    </csc-page>
 </template>
 
 <script>
     import CscPage  from '../../CscPage'
+    import CscNumberChip  from '../../card/CscNumberChip'
+    import { QChip, QCard, QCardSeparator, QCardTitle, QCardMain,
+        QIcon, QPopover, QList, QItem, QItemMain } from 'quasar-framework'
+
     export default {
+        mounted() {
+            this.$store.dispatch('pbxConfig/listSeats');
+        },
         data () {
             return {}
         },
         components: {
-            CscPage
+            CscPage,
+            CscNumberChip,
+            QChip,
+            QCard,
+            QCardSeparator,
+            QCardTitle,
+            QCardMain,
+            QIcon,
+            QPopover,
+            QList,
+            QItem,
+            QItemMain
+        },
+        computed: {
+            seats() {
+                return this.$store.getters['pbxConfig/seats'];
+            }
         }
     }
 </script>
 
 <style>
+    .pbx-seat .pbx-seat-title {
+        padding-left: 8px;
+    }
 </style>
