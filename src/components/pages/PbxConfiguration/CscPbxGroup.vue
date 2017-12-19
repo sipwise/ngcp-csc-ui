a8bf686974b6b048f4eb746be2b6e57629888a4b
Hans-Peter Herzog <hherzog@sipwise.com>
2017-12-19 11:38:03 +0100

    TT#28106 PBXConfig: As a Customer, I want to add new PBX Groups
    Change-Id: I44f20dc9c9ee6837e16210495bfe84b4d471489b

--- src/components/pages/PbxConfiguration/CscPbxGroup.vue
+++ src/components/pages/PbxConfiguration/CscPbxGroup.vue
@@ -1,5 +1,5 @@
 <template>
-    <q-card class="csc-card-collapsible csc-pbx-group">
+    <q-card class="csc-pbx-group">
         <q-card-title>
             <q-icon name="group" color="primary" size="22px"/>
             <span class="csc-pbx-group-title">{{ group.display_name }}</span>
@@ -9,7 +9,7 @@
         </q-card-title>
         <q-card-main>
             <q-field :label="$t('pbxConfig.groupName')">
-                <q-input v-model="name" readonly/>
+                <q-input v-model="name" readonly />
             </q-field>
             <q-field :label="$t('pbxConfig.extension')">
                 <q-input v-model="extension" readonly />
@@ -18,26 +18,25 @@
                 <q-select v-model="huntPolicy" :options="huntPolicyOptions" readonly radio />
             </q-field>
             <q-field :label="$t('pbxConfig.huntTimeout')">
-                <q-input v-model="huntTimeout" readonly suffix="s" readonly/>
+                <q-input v-model="huntTimeout" readonly suffix="seconds" readonly min="0" />
             </q-field>
             <q-field :label="$t('pbxConfig.primaryNumber')">
                 <q-input v-model="primaryNumber" readonly disabled />
             </q-field>
             <q-field :label="$t('pbxConfig.aliasNumbers')">
-                <q-select v-model="aliasNumbers" :options="allAliasNumbers" multiple chips readonly clearable />
+                <q-select v-model="aliasNumbers" :options="aliasNumberOptions" multiple chips readonly clearable />
             </q-field>
             <q-field :label="$t('pbxConfig.seats')">
-                <q-select v-model="assignedSeats" :options="allSeats" multiple chips readonly clearable />
+                <q-select v-model="seats" :options="seatOptions" multiple chips readonly clearable />
             </q-field>
         </q-card-main>
         <q-card-actions>
-
         </q-card-actions>
     </q-card>
 </template>
 
 <script>
-    import CscNumberChip  from '../../card/CscNumberChip'
+    import _ from 'lodash';
     import numberFilter from '../../../filters/number'
     import {
         QCard,
@@ -52,7 +51,11 @@
     } from 'quasar-framework'
     export default {
         name: 'csc-pbx-group',
-        props: ['group'],
+        props: ['group',
+            'huntPolicyOptions',
+            'aliasNumberOptions',
+            'seatOptions',
+        ],
         data () {
             return {}
         },
@@ -65,8 +68,7 @@
             QInput,
             QIcon,
             QSelect,
-            QChip,
-            CscNumberChip
+            QChip
         },
         computed: {
             name() {
@@ -86,96 +88,21 @@
             },
             aliasNumbers() {
                 let numbers = [];
-                if(this.group.alias_numbers) {
+                if(_.isArray(this.group.alias_numbers)) {
                     this.group.alias_numbers.forEach((number)=>{
                         numbers.push(number.number_id);
                     });
                 }
                 return numbers;
             },
-            huntPolicyOptions() {
-                return [
-                    {
-                        label: this.$t('pbxConfig.serialRinging'),
-                        value: 'serial'
-                    },
-                    {
-                        label: this.$t('pbxConfig.parallelRinging'),
-                        value: 'parallel'
-                    },
-                    {
-                        label: this.$t('pbxConfig.randomRinging'),
-                        value: 'random'
-                    },
-                    {
-                        label: this.$t('pbxConfig.circularRinging'),
-                        value: 'circular'
-                    }
-                ];
-            },
-            assignedSeats() {
+            seats() {
                 let seats = [];
-                if(this.group.seats) {
+                if(_.isArray(this.group.seats)) {
                     this.group.seats.forEach((seat)=>{
                         seats.push(seat.id);
                     });
                 }
                 return seats;
-            },
-            allSeats() {
-                let seats = [];
-                if(this.$store.getters['pbxConfig/seats']) {
-                    this.$store.getters['pbxConfig/seats'].forEach((seat)=>{
-                        seats.push({
-                            label: seat.display_name,
-                            sublabel: this.$t('pbxConfig.extension') + ': ' + seat.pbx_extension,
-                            value: seat.id
-                        });
-                    });
-                }
-                return seats;
-            },
-            allPrimaryNumbers() {
-                let numbers = [];
-                if(this.$store.getters['pbxConfig/numbers']) {
-                    this.$store.getters['pbxConfig/numbers'].forEach((number)=>{
-                        if(number.is_primary) {
-                            numbers.push({
-                                label: numberFilter(number),
-                                value: number.id
-                            });
-                        }
-                    });
-                }
-                return numbers;
-            },
-            allAliasNumbers() {
-                let numbers = [];
-                if(this.$store.getters['pbxConfig/numbers']) {
-                    this.$store.getters['pbxConfig/numbers'].forEach((number)=>{
-                        if(!number.is_primary) {
-                            let owner = this.$t('pbxConfig.allocatedByNobody');
-                            if(number.subscriber !== null && number.subscriber.display_name !== null &&
-                                number.subscriber.is_pbx_group) {
-                                owner = this.$t('pbxConfig.allocatedBy', {
-                                    type: this.$t('pbxConfig.group'),
-                                    name:  number.subscriber.display_name
-                                });
-                            } else if (number.subscriber !== null && number.subscriber.display_name !== null) {
-                                owner = this.$t('pbxConfig.allocatedBy', {
-                                    type: this.$t('pbxConfig.seat'),
-                                    name:  number.subscriber.display_name
-                                });
-                            }
-                            numbers.push({
-                                label: numberFilter(number),
-                                sublabel: owner,
-                                value: number.id
-                            });
-                        }
-                    });
-                }
-                return numbers;
             }
         }
     }
