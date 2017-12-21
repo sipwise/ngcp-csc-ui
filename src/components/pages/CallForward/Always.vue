abe6e17929e9d53e6f268bea1092e9170c460f4e
raxelsen <raxelsen@sipwise.com>
2017-12-21 16:18:42 +0100

    TT#28600 Implement Destinations Store
    What has been done:
    - TT#28603, CallForwarding: Implement the "Call Forward > Always"
      sub-module view
    - TT#28600, CallForwarding: Implement destinations store
    
    Change-Id: Id1e95dbaf1e55dba25f855ef9ad61352ededd5bb

--- src/components/pages/CallForward/Always.vue
+++ src/components/pages/CallForward/Always.vue
@@ -1,19 +1,57 @@
 <template>
-    <csc-page title="Always"></csc-page>
+    <csc-page title="Always">
+        <csc-destinations title="When I am online ..."
+			:destinations="destinations.everyone.always.online">
+        </csc-destinations>
+        <csc-destinations title="When I am busy ..."
+			:destinations="destinations.everyone.always.busy">
+        </csc-destinations>
+        <csc-destinations title="When I am offline ..."
+			:destinations="destinations.everyone.always.offline">
+        </csc-destinations>
+    </csc-page>
 </template>
 
 <script>
-    import CscPage  from '../../CscPage'
+    import CscPage from '../../CscPage'
+    import CscDestinations from './CscDestinations'
     export default {
         data () {
             return {
+				destinations: {
+					everyone: {
+						always: {
+							online: [
+								{ simple_destination: "1111", timeout: "300" },
+								{ simple_destination: "2222", timeout: "300" },
+								{ simple_destination: "3333", timeout: "300" },
+								{ simple_destination: "4444", timeout: "300" }
+							],
+							busy: [
+								{ simple_destination: "5555", timeout: "10" },
+								{ simple_destination: "6666", timeout: "10" },
+								{ simple_destination: "7777", timeout: "10" },
+								{ simple_destination: "8888", timeout: "10" }
+							],
+							offline: [
+								{ simple_destination: "9999", timeout: "30" },
+								{ simple_destination: "0000", timeout: "30" }
+							],
+						},
+						after_hours: {
+						}
+					},
+					list_a: {
+					}
+				}
             }
         },
         components: {
-            CscPage
+            CscPage,
+            CscDestinations
         }
     }
 </script>
 
-<style>
+<style lang="stylus">
 </style>
---
+++ src/components/pages/CallForward/CscDestinations.vue
@@ -0,0 +1,57 @@
+<template>
+    <q-card class="destination-card">
+        <q-card-title>
+            {{ title }}
+        </q-card-title>
+        <q-card-main>
+            <q-list no-border>
+                <q-item highlight v-for="(destination, index) in destinations">
+                    <div v-if="index == 0" class="dest-prefix-label">
+                        first ring
+                    </div>
+                    <div v-else-if="index == 1" class="dest-prefix-label">
+                        then forward to ...
+                    </div>
+                    <div v-else-if="index > 1" class="dest-prefix-label">
+                    </div>
+                    <div class="dest-row" style="display:inline-block;">
+                        <span>{{ destination.simple_destination }}</span>
+                        <span class="dest-ring-for"> and ring for </span>
+                        <span>{{ destination.timeout }}</span>
+                        <span class="dest-secs"> secs</span>
+                    </div>
+                </q-item>
+            </q-list>
+        </q-card-main>
+    </q-card>
+</template>
+
+<script>
+    import { QCard, QCardTitle, QCardMain,
+        QList, QItem } from 'quasar-framework'
+    export default {
+        name: 'csc-destinations',
+        props: [
+            'title',
+            'destinations'
+        ],
+        components: {
+            QCard,
+            QCardTitle,
+            QCardMain,
+            QList,
+            QItem
+        },
+        computed: {
+
+        },
+        methods: {
+        }
+    }
+</script>
+
+<style lang="stylus">
+.dest-prefix-label
+    display inline-block
+    width 160px
+</style>
