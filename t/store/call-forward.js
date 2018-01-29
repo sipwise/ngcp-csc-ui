8c77ac905ee9494c410dbe0c80da313c1f70a7de
raxelsen <raxelsen@sipwise.com>
2018-01-29 14:36:30 +0100

    TT#28056 List all company hours destinations
    What has been done:
    - TT#31193, CallForwarding: Create store mutation unit test
    - TT#31191, CallForwarding: Implement API requests
    - TT#31192, CallForwarding: Implement store
    - TT#31175, CallForwarding: Implement vue component
    
    Change-Id: I971e06cea0b930f57ede43b7a56c6766a790e651

--- t/store/call-forward.js
+++ t/store/call-forward.js
@@ -50,6 +50,36 @@ describe('CallForward', function(){
         };
         CallForwardModule.mutations.resetFormState(state);
         assert.deepEqual(state.form, data);
+
+    it('should load always company hours destinations', function(){
+        let state = {
+            alwaysCompanyHoursDestinations: [
+            ]
+        };
+        let data = {
+            busy: [],
+            offline: [{
+                destinations: [{
+                    "announcement_id": null,
+                    "destination": "sip:3333@192.168.178.23",
+                    "priority": 1,
+                    "simple_destination": "3333",
+                    "timeout": 60
+                },
+                {
+                    "announcement_id": null,
+                    "destination": "sip:2222@192.168.178.23",
+                    "priority": 1,
+                    "simple_destination": "2222",
+                    "timeout": 300
+                }],
+                id: 3,
+                name: "csc_destinationset_1"
+            }],
+            online: []
+        };
+        CallForwardModule.mutations.loadAlwaysCompanyHoursDestinations(state, data);
+        assert.deepEqual(state.alwaysCompanyHoursDestinations, data);
     });
 
 });
