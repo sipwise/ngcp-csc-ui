fcbd7f64ef32d106d148d32bdddd51a57e5c5d22
raxelsen <raxelsen@sipwise.com>
2018-02-01 11:42:56 +0100

    TT#28057 List all after hours destinations
    What has been done:
    - TT#31190, CallForwarding: Create store mutation unit test
    - TT#31188, CallForwarding: Implement API requests
    - TT#31189, CallForwarding: Implement store
    - TT#31174, CallForwarding: Implement vue component
    
    Change-Id: I79557be8c8cea4b8014e25afab213b8ab4d4acb8

--- src/components/pages/CallForward/AfterHours.vue
+++ src/components/pages/CallForward/AfterHours.vue
@@ -1,16 +1,29 @@
 <template>
     <csc-page :title="$t('pages.callForward.titles.afterHours')">
+        <csc-call-forward timeset="After Hours" :destinations="destinations">
+        </csc-call-forward>
     </csc-page>
 </template>
 
 <script>
+    import { mapState } from 'vuex'
     import CscPage from '../../CscPage'
+    import CscCallForward from './CscCallForward'
     export default {
         data () {
             return {}
         },
         components: {
-            CscPage
+            CscPage,
+            CscCallForward
+        },
+        created() {
+            this.$store.dispatch('callForward/loadAfterHoursEverybodyDestinations');
+        },
+        computed: {
+            ...mapState('callForward', {
+                destinations: 'afterHoursEverybodyDestinations'
+            })
         }
     }
 </script>
---
+++ src/components/pages/CallForward/CscCallForward.vue
@@ -0,0 +1,114 @@
+<template>
+    <q-card class="dest-card">
+        <csc-destinations :title="$t('pages.callForward.whenOnline')"
+            :group="destinations.online"
+            group-name="cfu"
+            :timeset="timeset"
+            icon="signal_wifi_4_bar">
+        </csc-destinations>
+        <csc-destinations :title="$t('pages.callForward.whenBusy')"
+            :group="destinations.busy"
+            group-name="cfb"
+            :timeset="timeset"
+            icon="record_voice_over">
+        </csc-destinations>
+        <csc-destinations :title="$t('pages.callForward.whenOffline')"
+            :group="destinations.offline"
+            group-name="cfna"
+            :timeset="timeset"
+            icon="signal_wifi_off">
+        </csc-destinations>
+    </q-card>
+</template>
+
+<script>
+    import numberFormat from '../../../filters/number-format'
+    import { mapState } from 'vuex'
+    import { startLoading, stopLoading,
+        showGlobalError, showToast } from '../../../helpers/ui'
+    import CscDestinations from './CscDestinations'
+    import { QCard } from 'quasar-framework'
+    export default {
+        name: 'csc-call-forward',
+        props: [
+            'timeset',
+            'destinations'
+        ],
+        data () {
+            return {
+            }
+        },
+        components: {
+            QCard,
+            CscDestinations
+        },
+        computed: {
+            ...mapState('callForward', {
+                removeDestinationState: 'removeDestinationState',
+                addDestinationState: 'addDestinationState',
+                changeDestinationState: 'changeDestinationState',
+                lastRemovedDestination: 'lastRemovedDestination',
+                lastAddedDestination: 'lastAddedDestination',
+                addDestinationError(state) {
+                    return state.addDestinationError ||
+                        this.$t('pages.callForward.addErrorMessage');
+                }
+            })
+        },
+        methods: {
+            reload(timeset) {
+                if (!timeset) {
+                    this.$store.dispatch('callForward/loadAlwaysEverybodyDestinations');
+                } else if (timeset === 'Company Hours') {
+                    this.$store.dispatch('callForward/loadCompanyHoursEverybodyDestinations');
+                } else if (timeset === 'After Hours') {
+                    this.$store.dispatch('callForward/loadAfterHoursEverybodyDestinations');
+                };
+            }
+        },
+        watch: {
+            removeDestinationState(state) {
+                if (state === 'requesting') {
+                    startLoading();
+                } else if (state === 'failed') {
+                    stopLoading();
+                    showGlobalError(this.removeDestinationError);
+                } else if (state === 'succeeded') {
+                    stopLoading();
+                    showToast(this.$t('pages.callForward.removeSuccessMessage', {
+                        destination: this.lastRemovedDestination
+                    }));
+                    this.reload(this.timeset);
+                }
+            },
+            addDestinationState(state) {
+                if (state === 'requesting') {
+                    startLoading();
+                } else if (state === 'failed') {
+                    stopLoading();
+                    showGlobalError(this.addDestinationError);
+                } else if (state === 'succeeded') {
+                    stopLoading();
+                    showToast(this.$t('pages.callForward.addDestinationSuccessMessage', {
+                        destination: this.lastAddedDestination
+                    }));
+                    this.reload(this.timeset);
+                }
+            },
+            changeDestinationState(state) {
+                if (state === 'requesting') {
+                    startLoading();
+                } else if (state === 'failed') {
+                    stopLoading();
+                    showGlobalError(this.changeDestinationError);
+                } else if (state === 'succeeded') {
+                    stopLoading();
+                    this.reload(this.timeset);
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="stylus">
+</style>
