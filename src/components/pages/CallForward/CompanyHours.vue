609df14db8ed90b92141f1baf9c9d90c37e5da49
raxelsen <raxelsen@sipwise.com>
2018-02-23 16:17:33 +0100

    TT#28060 Add time to company hours
    What has been done:
    - TT#32500, CallForwarding: Implement UI elements and flow
    - TT#31806, CallForwarding: Implement api requests
    - TT#32501, CallForwarding: Implement store and state management
    
    Change-Id: I7ba7c2b5115f10480db4f560e39ffa43205c92af

--- src/components/pages/CallForward/CompanyHours.vue
+++ src/components/pages/CallForward/CompanyHours.vue
@@ -5,60 +5,73 @@
             <csc-call-forward-destinations timeset="Company Hours" :destinations="destinations">
             </csc-call-forward-destinations>
         </div>
-        <div v-else-if="timesetHasDuplicate && timesetExists">
-            <q-alert color="red"
-                icon="date_range"
-                enter="bounceInLeft"
-                leave="bounceOutRight"
-                appear>
-                    {{ $t('pages.callForward.times.companyHoursDuplicate') }}
-            </q-alert>
-        </div>
-        <div v-else-if="!timesetIsCompatible && timesetExists">
-            <q-alert color="red"
-                icon="date_range"
-                enter="bounceInLeft"
-                leave="bounceOutRight"
-                appear>
-                    {{ $t('pages.callForward.times.companyHoursIncompatible') }}
-            </q-alert>
-        </div>
-        <div v-else-if="timesetHasReverse && timesetExists">
-            <q-alert color="red"
-                icon="date_range"
-                enter="bounceInLeft"
-                leave="bounceOutRight"
-                appear>
-                    {{ $t('pages.callForward.times.companyHoursReverse') }}
-            </q-alert>
-        </div>
-        <div v-else>
-            <q-alert color="warning"
-                icon="date_range"
-                enter="bounceInLeft"
-                leave="bounceOutRight"
-                appear>
-                    {{ $t('pages.callForward.times.companyHoursNotDefined') }}
-            </q-alert>
-        </div>
+        <q-card class="times-card">
+            <div v-if="timesetHasDuplicate">
+                <q-alert color="red"
+                    v-model="showAlertDuplicate"
+                    icon="date_range"
+                    :actions="[{ label: $t('pages.callForward.times.resetCompanyHours'), handler: resetCompanyHours }]"
+                    appear>
+                        {{ $t('pages.callForward.times.companyHoursDuplicate') }}
+                </q-alert>
+            </div>
+            <div v-if="!timesetIsCompatible">
+                <q-alert color="red"
+                    v-model="showAlertCompatible"
+                    icon="date_range"
+                    :actions="[{ label: $t('pages.callForward.times.resetCompanyHours'), handler: resetCompanyHours }]"
+                    appear>
+                        {{ $t('pages.callForward.times.companyHoursIncompatible') }}
+                </q-alert>
+            </div>
+            <div v-if="timesetHasReverse">
+                <q-alert color="red"
+                    v-model="showAlertReverse"
+                    icon="date_range"
+                    :actions="[{ label: $t('pages.callForward.times.resetCompanyHours'), handler: resetCompanyHours }]"
+                    appear>
+                        {{ $t('pages.callForward.times.companyHoursReverse') }}
+                </q-alert>
+            </div>
+            <div v-if="!timesetExists">
+                itjtjtj
+                <q-alert color="warning"
+                    v-model="showAlertDefined"
+                    icon="date_range"
+                    :actions="[{ label: $t('pages.callForward.times.addCompanyHours'), handler: addCompanyHours }]"
+                    class="timeset-alert"
+                    appear>
+                        {{ $t('pages.callForward.times.companyHoursNotDefined') }}
+                </q-alert>
+            </div>
+            <csc-add-time-form type="new" :title="$t('pages.callForward.times.addCompanyHours')" timeset="Company Hours"></csc-add-time-form>
+            <q-btn @click="logDefinedState()">LOG DEFINED STATE</q-btn>
+        </q-card>
     </csc-page>
 </template>
 
 <script>
     import { mapState } from 'vuex'
-    import { QAlert } from 'quasar-framework'
+    import { QAlert, QCard, QBtn } from 'quasar-framework'
     import CscPage from '../../CscPage'
+    import { startLoading, stopLoading,
+        showGlobalError, showToast } from '../../../helpers/ui'
     import CscCallForwardDestinations from './CscCallForwardDestinations'
     import CscCallForwardTimes from './CscCallForwardTimes'
+    import CscAddTimeForm from './CscAddTimeForm'
     export default {
         data () {
-            return {}
+            return {
+            }
         },
         components: {
             CscPage,
             CscCallForwardDestinations,
             CscCallForwardTimes,
-            QAlert
+            CscAddTimeForm,
+            QAlert,
+            QCard,
+            QBtn
         },
         created() {
             this.$store.dispatch('callForward/loadCompanyHoursEverybodyDestinations');
@@ -67,17 +80,89 @@
             });
         },
         computed: {
-            ...mapState('callForward', [
-                'destinations',
-                'timesetTimes',
-                'timesetIsCompatible',
-                'timesetHasReverse',
-                'timesetHasDuplicate',
-                'timesetExists'
-            ])
+            ...mapState('callForward', {
+                destinations: 'destinations',
+                timesetTimes: 'timesetTimes',
+                timesetIsCompatible: 'timesetIsCompatible',
+                timesetHasReverse: 'timesetHasReverse',
+                timesetHasDuplicate: 'timesetHasDuplicate',
+                timesetExists: 'timesetExists',
+                resetTimeState: 'resetTimeState',
+                resetTimeError(state) {
+                    return state.resetTimeError ||
+                        this.$t('pages.callForward.times.resetErrorMessage');
+                },
+                addTimeState: 'addTimeState',
+                addTimeError(state) {
+                    return state.addTimeError ||
+                        this.$t('pages.callForward.times.addTimeErrorMessage');
+                },
+                // TODO: Convert these to computed with get/set, and try again
+                showAlertDuplicate: 'showAlertDuplicate',
+                showAlertCompatible: 'showAlertCompatible',
+                showAlertReverse: 'showAlertReverse'
+            }),
+            // TODO: Replace dispatch with commit where applicable
+            showAlertDefined: {
+                get() {
+                    return this.$store.state.callForward.showAlertDefined;
+                },
+                set(value) {
+                    this.$store.dispatch('callForward/setShowAlertDefined', value);
+                }
+            }
+        },
+        methods: {
+            logDefinedState() {
+                console.log('showAlertDefined', this.showAlertDefined);
+            },
+            resetCompanyHours() {
+                this.$store.dispatch('callForward/resetTimesetByName', 'Company Hours');
+            },
+            addCompanyHours() {
+                this.$store.dispatch('callForward/setActiveTimeForm', true);
+            }
+        },
+        watch: {
+            resetTimeState(state) {
+                if (state === 'requesting') {
+                    startLoading();
+                } else if (state === 'failed') {
+                    stopLoading();
+                    showGlobalError(this.resetTimeError);
+                } else if (state === 'succeeded') {
+                    stopLoading();
+                    //this.$store.dispatch('callForward/setShowAlertDefined', true);
+                    showToast(this.$t('pages.callForward.times.resetSuccessMessage'));
+                    this.$store.dispatch('callForward/loadTimesetTimes', {
+                        timeset: 'Company Hours'
+                    });
+                }
+            },
+            addTimeState(state) {
+                if (state === 'requesting') {
+                    startLoading();
+                } else if (state === 'failed') {
+                    stopLoading();
+                    showGlobalError(this.addTimeError);
+                } else if (state === 'succeeded') {
+                    stopLoading();
+                    // TODO: Also dispatch in other states?
+                    this.$store.dispatch('callForward/setActiveTimeForm', false);
+                    showToast(this.$t('pages.callForward.times.addTimeSuccessMessage'));
+                    this.$store.dispatch('callForward/loadTimesetTimes', {
+                        timeset: 'Company Hours'
+                    });
+                }
+            }
         }
     }
 </script>
 
-<style>
+<style lang="stylus">
+@import '~variables'
+    .times-card
+        padding 0 15px
+        .q-alert-container
+            padding 15px 0
 </style>
---
+++ src/components/pages/CallForward/CscAddTimeForm.vue
@@ -0,0 +1,157 @@
+<template>
+    <div v-if="activeTimeForm" class="add-times">
+        <q-card-title v-if="type === 'new'" class="times-title">
+            {{ title }}
+        </q-card-title>
+        <q-card-main>
+            <div class="row no-wrap">
+                <q-field class="col-8">
+                    <q-select v-model="selectedWeekday"
+                        :options="selectOptions" />
+                </q-field>
+                <q-field class="col-2"
+                    :error="timeHasError"
+                    :error-label="$t('pages.callForward.times.selectValidTime')">
+                        <q-datetime
+                            color="primary"
+                            v-model="timeFromConverted"
+                            align="right"
+                            type="time"
+                            format24h />
+                </q-field>
+                <q-field class="col-2"
+                    :error="timeHasError"
+                    :error-label="$t('pages.callForward.times.selectValidTime')">
+                        <q-datetime
+                            color="primary"
+                            v-model="timeToConverted"
+                            align="right"
+                            type="time"
+                            format24h />
+                </q-field>
+            </div>
+            <q-btn flat dark @click="disableForm()">{{ $t('buttons.cancel') }}</q-btn>
+            <q-btn flat color="primary" icon-right="fa-save" @click="addTime()">{{ $t('buttons.save') }}</q-btn>
+        </q-card-main>
+    </div>
+</template>
+
+<script>
+    import { mapState } from 'vuex'
+    import { QField, QSelect, QDatetime, QCardTitle,
+        QCardMain, QBtn, date } from 'quasar-framework'
+    export default {
+        name: 'csc-add-time-form',
+        props: [
+            'type',
+            'title',
+            'timeset'
+        ],
+        data () {
+            return {
+                selectOptions: [
+                    { label: this.$t('pages.callForward.times.sunday'), value: 1 },
+                    { label: this.$t('pages.callForward.times.monday'), value: 2 },
+                    { label: this.$t('pages.callForward.times.tuesday'), value: 3 },
+                    { label: this.$t('pages.callForward.times.wednesday'), value: 4 },
+                    { label: this.$t('pages.callForward.times.thursday'), value: 5 },
+                    { label: this.$t('pages.callForward.times.friday'), value: 6 },
+                    { label: this.$t('pages.callForward.times.saturday'), value: 7 }
+                ],
+                selectedWeekday: 1
+            }
+        },
+        components: {
+            QField,
+            QSelect,
+            QDatetime,
+            QCardTitle,
+            QCardMain,
+            QBtn,
+            date
+        },
+        computed: {
+            ...mapState('callForward', {
+                activeTimeForm: 'activeTimeForm'
+            }),
+            timeFrom: {
+                get() {
+                    return this.$store.state.callForward.addTimeForm.timeFrom;
+                },
+                set(value) {
+                    this.$store.dispatch('callForward/setTimeFrom', value);
+                }
+            },
+            timeTo: {
+                get() {
+                    return this.$store.state.callForward.addTimeForm.timeTo;
+                },
+                set(value) {
+                    this.$store.dispatch('callForward/setTimeTo', value);
+                }
+            },
+            timeFromConverted: {
+                get() {
+                    return date.buildDate({
+                        hours: this.timeFrom.split(':')[0],
+                        minutes: this.timeFrom.split(':')[1]
+                    });
+                },
+                set(value) {
+                    this.timeFrom = date.formatDate(value, 'HH:mm');
+                }
+            },
+            timeToConverted: {
+                get() {
+                    return date.buildDate({
+                        hours: this.timeTo.split(':')[0],
+                        minutes: this.timeTo.split(':')[1]
+                    });
+                },
+                set(value) {
+                    this.timeTo = date.formatDate(value, 'HH:mm');
+                }
+            },
+            timeHasError() {
+                let timeToHour = parseInt(this.timeTo.split(':')[0]);
+                let timeFromHour = parseInt(this.timeFrom.split(':')[0]);
+                let timeToMinute = parseInt(this.timeTo.split(':')[1]);
+                let timeFromMinute = parseInt(this.timeFrom.split(':')[1]);
+                let hoursReverse = timeToHour < timeFromHour || timeToMinute < timeFromMinute;
+                let sameTime = this.timeTo === this.timeFrom;
+                return hoursReverse || sameTime;
+            }
+        },
+        methods: {
+            disableForm() {
+                this.$store.dispatch('callForward/setShowAlertDefined', true);
+                this.$store.dispatch('callForward/setActiveTimeForm', false);
+            },
+            addTime() {
+                if (this.type === "new") {
+                    this.$store.dispatch('callForward/createTimesetWithTime', {
+                        time: [{ from: this.timeFrom, to: this.timeTo}],
+                        weekday: this.selectedWeekday,
+                        name: this.timeset
+                    });
+                } else if (this.type === "existing") {
+                    this.$store.dispatch('callForward/appendTimeToTimeset', {
+                        time: [{ from: this.timeFrom, to: this.timeTo}],
+                        weekday: this.selectedWeekday,
+                        name: this.timeset
+                    })
+                };
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+@import '~variables'
+    .add-times
+        margin-right 30px
+    .q-field
+        margin 5px 0
+    .q-card-main
+        padding-bottom 8px
+</style>
