e4b18e7820b2defc2ee1295e50ba3bd3c5fd2269
raxelsen <raxelsen@sipwise.com>
2018-03-12 15:54:01 +0100

    TT#35422 Destinations by sourcesets, API methods
    Change-Id: I0747c45778a9db068f77422f316eb7522e3cb14c

--- src/components/pages/CallForward/Always.vue
+++ src/components/pages/CallForward/Always.vue
@@ -1,37 +1,150 @@
 <template>
     <csc-page :title="$t('pages.callForward.titles.always')">
-        <csc-call-forward-destinations :timeset="alwaysTimeset" :destinations="destinations">
-        </csc-call-forward-destinations>
+        <q-tabs>
+            <q-tab default slot="title" name="Everybody" icon="people" label="Everybody" />
+            <q-tab v-for="sourceset in namedDestinations" :count="destinationsCount(sourceset.destinationGroups)" :key="sourceset.sourcesetId" slot="title" :name="sourceset.sourcesetName" icon="people" :label="sourceset.sourcesetName" />
+            <q-tab-pane name="Everybody">
+                <csc-call-forward-destinations :timeset="alwaysTimeset" :destinations="everybodySourceset.destinationGroups" />
+            </q-tab-pane>
+            <q-tab-pane v-for="sourceset in namedDestinations" :key="sourceset.sourcesetId" :name="sourceset.sourcesetName">
+                <div class="sources-section">
+                    <div class="sources-title">
+                        <q-icon name="contact_phone" class="sources-icon" />
+                        Sources
+                    </div>
+                    <q-list no-border>
+                        <q-item highlight separator v-for="source in sourcesetSources(sourceset.sourcesetId)">
+                            {{ source.source }}
+                        </q-item>
+                    </q-list>
+                </div>
+                <csc-call-forward-destinations :timeset="alwaysTimeset" :destinations="sourceset.destinationGroups" />
+            </q-tab-pane>
+        </q-tabs>
+        <!--<q-btn @click="getState()">-->
+            <!--LOG STATE-->
+        <!--</q-btn>-->
     </csc-page>
 </template>
 
 <script>
     import { mapState } from 'vuex'
+    import { QTabs, QTab, QTabPane, QBtn, QList, QItem, QIcon } from 'quasar-framework'
     import CscPage from '../../CscPage'
     import CscCallForwardDestinations from './CscCallForwardDestinations'
     export default {
         data () {
-            return {}
+            return {
+                selectedTab: "everybody"
+            }
         },
         components: {
             CscPage,
-            CscCallForwardDestinations
+            CscCallForwardDestinations,
+            QTabs,
+            QTab,
+            QTabPane,
+            QBtn,
+            QList,
+            QItem,
+            QIcon
         },
         created() {
-            this.$store.dispatch('callForward/loadEverybodyDestinations', {
+            this.$store.dispatch('callForward/loadDestinations', {
                 timeset: null
             });
+            this.$store.dispatch('callForward/loadSourcesets');
         },
         computed: {
             ...mapState('callForward', [
-                'destinations'
+                'destinations',
+                'sourcesets'
             ]),
+            // TODO: Move these to getters instead
+            everybodySourceset() {
+                return this.destinations.filter((sourceset, index) => {
+                    return index === 0;
+                })[0];
+            },
+            namedDestinations() {
+                return this.destinations.filter((sourceset, index) => {
+                    return index > 0;
+                });
+            },
             alwaysTimeset() {
                 return null;
+            }
+        },
+        methods: {
+            sourcesetSources(id) {
+                return this.sourcesets.filter((sourceset) => {
+                    return sourceset.id === id;
+                })[0].sources;
+            },
+            destinationsCount(groups) {
+                let busyLength = 0;
+                let offlineLength = 0;
+                let onlineLength = 0;
+                if (groups.busy.length > 0) {
+                    let count = 0;
+                    groups.busy.forEach((destinationSet) => {
+                        count += destinationSet.destinations.length;
+                    });
+                    busyLength = count;
+                }
+                if (groups.offline.length > 0) {
+                    let count = 0;
+                    groups.offline.forEach((destinationSet) => {
+                        count += destinationSet.destinations.length;
+                    });
+                    offlineLength = count;
+                }
+                if (groups.online.length > 0) {
+                    let count = 0;
+                    groups.online.forEach((destinationSet) => {
+                        count += destinationSet.destinations.length;
+                    });
+                    onlineLength = count;
+                }
+                return busyLength + offlineLength + onlineLength;
+            },
+            tabId(id) {
+                if (id === null) {
+                    return 0;
+                }
+                else {
+                    return id;
+                }
+            },
+            tabName(name) {
+                if (name === null) {
+                    return 'Everybody';
+                }
+                else {
+                    return name;
+                }
+            },
+            getState() {
+                 console.log('destinations', this.destinations);
+                 console.log('sourcesets', this.sourcesets);
             }
         }
     }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+    @import '../../../themes/quasar.variables.styl'
+
+    .q-item-highlight.csc-destination:hover
+        background-color lighten($primary, 70%)
+
+    .sources-section
+        padding 30px 0 20px 0
+
+    .sources-title
+        color $secondary
+        font-size 16px
+
+    .sources-icon
+        margin-right 5px
 </style>
--- src/store/call-forward.js
+++ src/store/call-forward.js
@@ -3,7 +3,8 @@
 
 import _ from 'lodash';
 import { i18n } from '../i18n';
-import { getSourcesets,
+import {
+    getSourcesets,
     getDestinationsets,
     getTimesets,
     getMappings,
@@ -20,7 +21,10 @@ import { getSourcesets,
     deleteTimesetById,
     resetTimesetByName,
     createTimesetWithTime,
-    appendTimeToTimeset } from '../api/call-forward';
+    appendTimeToTimeset,
+    loadDestinations,
+    getSourcesetById
+} from '../api/call-forward';
 
 const RequestState = {
     button: 'button',
@@ -33,14 +37,26 @@ export default {
     namespaced: true,
     state: {
         mappings: null,
-        sourcesets: null,
+        sourcesets: [],
+        sourceset: [],
         timesets: null,
         destinationsets: null,
-        destinations: {
-            online: [],
-            busy: [],
-            offline: []
-        },
+        //destinations: {
+            //online: [],
+            //busy: [],
+            //offline: []
+        //},
+        destinations: [
+            {
+                sourcesetId: null,
+                sourcesetName: null,
+                destinationGroups: {
+                    online: [],
+                    busy: [],
+                    offline: []
+                }
+            }
+        ],
         removeDestinationState: RequestState.button,
         removeDestinationError: null,
         lastRemovedDestination: null,
@@ -260,6 +276,9 @@ export default {
             state.timesetHasReverse = false;
             state.timesetHasDuplicate = false;
             state.addTimeState = RequestState.button;
+        },
+        setSourceset(state, result) {
+            state.sourceset = result;
         }
     },
     actions: {
@@ -513,6 +532,25 @@ export default {
                 }).catch((err) => {
                     context.commit('addTimeFailed', err.message);
                 });
+        },
+        loadDestinations(context, options) {
+            loadDestinations({
+                timeset: options.timeset,
+                subscriberId: context.getters.getSubscriberId
+            }).then((result) => {
+                context.commit('loadDestinations', result);
+            }).catch((err) => {
+                // TODO: Error handling
+                console.log(err);
+            });
+        },
+        getSourcesetById(context, id) {
+            getSourcesetById(id).then((result) => {
+                context.commit('setSourceset', result);
+            }).catch((err) => {
+                // TODO: Error handling
+                console.log(err);
+            });
         }
     }
 };
