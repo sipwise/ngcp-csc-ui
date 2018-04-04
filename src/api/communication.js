d3211cb5a0407fc5b50475d405e637a7fb8a56d6
raxelsen <raxelsen@sipwise.com>
2018-04-04 17:54:45 +0200

    TT#20311 Customer wants to send a fax
    What has been done:
    - TT#34863, Global: Implement vuex store actions, mutations and getters
    - TT#34864, Global: Implement vue components
    - TT#34862, Global: Implement api requests
    
    Change-Id: Ib547cc125271222d005ca131113086739ae3220f

---
+++ src/api/communication.js
@@ -0,0 +1,17 @@
+
+import Vue from 'vue';
+
+export function createFax(options, subscriberId) {
+    return new Promise((resolve, reject) => {
+        let headers = {
+            'Content-Type': 'application/json',
+        };
+        let mergedParams = Object.assign(options, subscriberId);
+        let payload = JSON.stringify(mergedParams);
+        Vue.http.post('/api/faxes/', payload, { headers: headers }).then(() => {
+            resolve();
+        }).catch((err)=>{
+            reject(err);
+        });
+    });
+}
