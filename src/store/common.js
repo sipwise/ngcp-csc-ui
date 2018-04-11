44d01b997db5d7dcf66dcb51fa92dfe1513c61e5
Hans-Peter Herzog <hherzog@sipwise.com>
2018-04-11 12:52:24 +0200

    TT#20517 PBXConfig: As a Customer, I want to edit PBX Seats
    Change-Id: I48808fdb6931a4bf2737d9f3a2a23503b24353fd

--- src/store/common.js
+++ src/store/common.js
@@ -1,24 +1,8 @@
 'use strict';
 
 export const RequestState = {
-    requesting: 'requesting',
-    succeeded: 'succeeded',
-    failed: 'failed'
-};
-
-export const ListState = {
     initiated: 'initiated',
     requesting: 'requesting',
     succeeded: 'succeeded',
     failed: 'failed'
 };
-
-export const AddState = {
-    button: 'button',
-    input: 'input',
-    requesting: 'requesting',
-    succeeded: 'succeeded',
-    failed: 'failed'
-};
-
-export const RemoveState = ListState;
--- src/store/pbx-config/getters.js
+++ src/store/pbx-config/getters.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import _ from 'lodash'
+import { RequestState } from '../common'
 
 export default {
     groups(state) {
@@ -36,19 +37,67 @@ export default {
         }
         return aliasNumbers;
     },
-    listItemUpdating(state) {
-        return state.listItemUpdating;
+    isListRequesting(state) {
+        return state.listState === RequestState.requesting;
     },
-    listItemUpdateState(state) {
-        return state.listItemUpdateState;
+    listState(state) {
+        return state.listState;
     },
-    listItemUpdateError(state) {
-        return state.listItemUpdateError;
+    listError(state) {
+        return state.listError;
     },
     listLoadingSilently(state) {
         return (state.listLoadingSilently === true);
     },
     pilotId(state) {
         return state.pilot.id;
+    },
+    isAdding(state) {
+        return state.addState === RequestState.requesting;
+    },
+    addState(state) {
+        return state.addState;
+    },
+    addItem(state) {
+        return state.addItem;
+    },
+    addError(state) {
+        return state.addError;
+    },
+    isUpdating(state) {
+        return state.updateState === RequestState.requesting;
+    },
+    updateState(state) {
+        return state.updateState;
+    },
+    updateItem(state) {
+        return state.updateItem;
+    },
+    updateItemId(state, getters) {
+        if(_.isObject(getters.updateItem)) {
+            return getters.updateItem.id;
+        }
+        return null;
+    },
+    updateError(state) {
+        return state.updateError;
+    },
+    isRemoving(state) {
+        return state.removeState === RequestState.requesting;
+    },
+    removeState(state) {
+        return state.removeState;
+    },
+    removeItem(state) {
+        return state.removeItem;
+    },
+    removeItemId(state, getters) {
+        if(_.isObject(getters.removeItem)) {
+            return getters.removeItem.id;
+        }
+        return null;
+    },
+    removeError(state) {
+        return state.removeError;
     }
 }
