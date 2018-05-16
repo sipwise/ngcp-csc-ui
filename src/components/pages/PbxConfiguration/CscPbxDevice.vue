f82a1d80ae151dba607545c35fec56f0e03faa57
Hans-Peter Herzog <hherzog@sipwise.com>
2018-05-16 09:19:33 +0200

    TT#35501 PBXConfig: As a Customer, I want see all buttons and assigned seats of a device item
    Change-Id: I86d48c11bcb3fdda2193df2d00de61d24123467a

--- src/components/pages/PbxConfiguration/CscPbxDevice.vue
+++ src/components/pages/PbxConfiguration/CscPbxDevice.vue
@@ -1,36 +1,46 @@
 <template>
-    <q-card class="csc-entity csc-pbx-device shadow-1">
-        <q-card-title>
-            <q-icon name="fa-fax" color="secondary" size="24px"/>
-            <span class="csc-entity-title-text">{{ device.station_name }}</span>
-            <q-btn :icon="titleIcon" :small="isMobile" color="primary" slot="right" flat @click="toggleMain()" />
-        </q-card-title>
-        <q-card-main v-if="expanded">
-            <q-field :label="$t('pbxConfig.deviceStationName')">
-                <q-input v-model="device.station_name" readonly />
-            </q-field>
-            <q-field :label="$t('pbxConfig.deviceIdentifier')">
-                <q-input v-model="device.identifier" readonly />
-            </q-field>
-            <q-field :label="$t('pbxConfig.deviceModel')">
-                <!--<q-select v-model="device.model.id" :options="modelOptions" clearable disable />-->
-                <p>{{ name }}</p>
-                <div class="csc-pbx-device-image">
-                    <img v-show="hasFrontImage" ref="modelImage" :src="frontImageUrl" />
-                </div>
-            </q-field>
-        </q-card-main>
+    <q-item class="csc-entity csc-pbx-device">
+        <q-item-side v-if="!expanded">
+            <q-item-tile avatar>
+                <img :src="frontImageUrl" />
+            </q-item-tile>
+        </q-item-side>
+        <q-item-main>
+            <q-item-tile v-if="!expanded" label>{{ device.station_name }}</q-item-tile>
+            <q-item-tile v-if="!expanded" sublabel><span class="gt-sm">Model: </span>{{ name }}</q-item-tile>
+            <q-item-tile v-if="!expanded" sublabel><span class="gt-sm">MAC address: </span>{{ device.identifier }}</q-item-tile>
+            <q-item-tile v-if="expanded" class="csc-pbx-device-content">
+                <q-field :label="$t('pbxConfig.deviceStationName')">
+                    <q-input v-model="device.station_name" readonly />
+                </q-field>
+                <q-field :label="$t('pbxConfig.deviceIdentifier')">
+                    <q-input v-model="device.identifier" readonly />
+                </q-field>
+                <q-field :label="$t('pbxConfig.deviceModel')">
+                    <p>{{ name }}</p>
+                </q-field>
+                <csc-pbx-device-config :device="device" />
+            </q-item-tile>
+        </q-item-main>
+        <q-item-side right>
+            <q-item-tile>
+                <q-btn :icon="titleIcon" :big="isMobile" color="primary" slot="right" flat @click="toggleMain()" />
+            </q-item-tile>
+        </q-item-side>
         <q-inner-loading :visible="loading">
             <q-spinner-mat size="60px" color="primary"></q-spinner-mat>
         </q-inner-loading>
-    </q-card>
+    </q-item>
 </template>
+
 
 <script>
 
     import _ from 'lodash'
     import { QCard, QCardTitle, QCardMain, QCollapsible,
-        QIcon, QField, QInput, QSelect, QBtn, QInnerLoading, QSpinnerMat } from 'quasar-framework'
+        QIcon, QField, QInput, QSelect, QBtn, QInnerLoading, QSpinnerMat, QCardMedia,
+        QItem, QItemMain, QItemSide, QItemTile, Platform } from 'quasar-framework'
+    import CscPbxDeviceConfig from './CscPbxDeviceConfig'
     export default {
         name: 'csc-pbx-device',
         props: [
@@ -39,15 +49,20 @@
             'modelOptions'
         ],
         components: {
-            QCard, QCardTitle, QCardMain, QCollapsible,
-            QIcon, QField, QInput, QSelect, QBtn, QInnerLoading, QSpinnerMat
+            CscPbxDeviceConfig, QCard, QCardTitle, QCardMain, QCollapsible,
+            QIcon, QField, QInput, QSelect, QBtn, QInnerLoading, QSpinnerMat, QCardMedia,
+            QItem, QItemMain, QItemSide, QItemTile, Platform
         },
         data () {
             return {
-                expanded: false
+                expanded: false,
+                modalOpened: false
             }
         },
         computed: {
+            isMobile() {
+                return Platform.is.mobile;
+            },
             titleIcon() {
                 if(!this.expanded) {
                     return 'keyboard arrow down';
@@ -80,8 +95,22 @@
 <style lang="stylus" rel="stylesheet/stylus">
     @import '../../../themes/app.common'
 
-    .csc-pbx-device-image
-        width 100%
-        img
-            width 100%
+    .csc-pbx-device
+
+        .csc-pbx-device-content
+            padding-top: 32px;
+
+        .q-item-side.q-item-side-right
+            position absolute
+            right 10px
+
+        .q-item-avatar
+            img
+                border-radius 0
+
+        .q-item-label
+            font-size 18px
+            font-weight 400
+            letter-spacing normal
+            line-height 1.8rem
 </style>
---
+++ src/config.js
@@ -0,0 +1,5 @@
+
+export default {
+    baseHttpUrl: 'https://192.168.178.33',
+    baseWsUrl: 'wss://192.168.178.33'
+}
