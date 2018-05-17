3ebb370a33aaa4006f0b5085b4d85daf2c3cad37
raxelsen <raxelsen@sipwise.com>
2018-05-17 12:32:36 +0200

    TT#32966 Update Conversations List After Call
    What is remaining:
    - TT#37310, Fix infinite scroll component update mechanism
    - TT#37313, Implement update of infinite scroll on call ended event
    
    Change-Id: Ib3ffd236d64a7948252a4d2e220fe7e50965977f

--- src/components/CscConversations.vue
+++ src/components/CscConversations.vue
@@ -1,11 +1,20 @@
 <template>
     <div>
-        <q-infinite-scroll :handler="loadMore" :offset=1 ref="infinite">
-            <csc-conversation v-for="(conversation, index) in conversations"
-                :conversation="conversation" :key="conversation._id">
-            </csc-conversation>
-            <div slot="message" class="row justify-center">
-                <q-spinner-dots :size="40"></q-spinner-dots>
+        <q-infinite-scroll
+            :handler="loadMore"
+            :offset=1
+            ref="infinite"
+        >
+            <csc-conversation
+                v-for="(conversation, index) in conversations"
+                :conversation="conversation"
+                :key="conversation._id"
+            />
+            <div
+                slot="message"
+                class="row justify-center"
+            >
+                <q-spinner-dots :size="40" />
             </div>
         </q-infinite-scroll>
     </div>
@@ -13,7 +22,15 @@
 
 <script>
     import CscConversation from './CscConversation'
-    import { QInfiniteScroll, QSpinnerDots } from 'quasar-framework'
+    import { mapGetters } from 'vuex'
+    import {
+		QInfiniteScroll,
+		QSpinnerDots,
+		dom,
+		scroll
+	} from 'quasar-framework'
+	const { viewport } = dom
+	const { getScrollPosition, setScrollPosition } = scroll
     export default {
         name: 'csc-conversations',
         props: ['conversations'],
@@ -22,8 +39,22 @@
             QInfiniteScroll,
             QSpinnerDots
         },
+        computed: {
+            ...mapGetters('call', {
+                callEnded: 'isEnded'
+            })
+        },
         methods: {
+			reloadConversations() {
+				this.$store.dispatch('conversations/reloadConversations');
+			},
             loadMore(index, done) {
+				let { height, width } = viewport();
+				this.$refs.infinite.resume();
+                console.log('index', index);
+                console.log('height', height);
+                console.log('width', width);
+                console.log('getScrollPosition', getScrollPosition(window));
                 this.$store.dispatch('conversations/loadConversations')
                     .then(() => {
                         done();
@@ -31,6 +62,14 @@
                         done();
                         this.$refs.infinite.stop();
                     });
+            }
+        },
+        watch: {
+            callEnded() {
+                setScrollPosition(window, 0); // Might need to be conditional, or try behavior without
+                setTimeout(() => {
+					this.reloadConversations();
+                }, 9000);
             }
         }
     }
