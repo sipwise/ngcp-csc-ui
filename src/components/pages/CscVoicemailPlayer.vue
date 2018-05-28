cbd74a9fdba2569fd410d336c3b664780a0ceab4
Tijana Maksimovic <tmaksimovic@sipwise.com>
2018-05-28 14:13:45 +0200

    TT#37301 Create custom audio player component
    Change-Id: I66951f491f5d9cdbd100f941bc235d4e0c01c6fa

---
+++ src/components/pages/CscVoicemailPlayer.vue
@@ -0,0 +1,16 @@
+<template>
+</template>
+
+<script>
+    export default {
+        name: 'csc-voicemail-player',
+        props: [
+        ],
+        components: {
+        }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    @import '../../themes/app.common'
+</style>
