4be4e82c2579c85590e5209181d6d3612b5aa065
Hans-Peter Herzog <hherzog@sipwise.com>
2018-07-06 13:45:56 +0200

    TT#39948 Reminder: Refactor api methods, component, mutations and actions
    Change-Id: Ic9308c71bfc45bc34d8e4d910fc655453a664a04

--- src/api/reminder.js
+++ src/api/reminder.js
@@ -1,68 +1,127 @@
-import Vue from 'vue';
 
-export function createReminder(id) {
+import _ from 'lodash'
+import Vue from 'vue'
+import {
+    getList,
+    patchReplace
+} from './common'
+
+export function createReminder(subscriberId) {
     return new Promise((resolve, reject) => {
-        var data = {
-            subscriber_id: id,
+        Vue.http.post('api/reminders/', {
+            subscriber_id: subscriberId,
             time: '00:00',
             recur: 'never',
             active: false
-        };
-        Vue.http.post('api/reminders/', data).then((result) => {
-            var reminderID = result.headers.get('Location').split('/')[3];
-            resolve(reminderID);
-        }).catch((err) => {
-            reject(err);
-        });
-    });
-}
-
-export function getReminder(id) {
-    return new Promise((resolve, reject) => {
-        Vue.http.get('api/reminders/', {
-            params: {
-                supplier_id: id
-            }
         }).then((result) => {
-            resolve(JSON.parse(result.body));
+            let parts = result.headers.get('Location').split('/');
+            resolve(_.last(parts));
         }).catch((err) => {
             reject(err);
         });
     });
 }
 
-export function enableReminder(id) {
-    return patchReminder(id, '/active', true);
-}
-
-export function disableReminder(id) {
-    return patchReminder(id, '/active', false);
-}
-
-export function setTime(id, time) {
-    return patchReminder(id, '/time', time);
-}
-
-export function setRecurrence(id, recurrence) {
-    return patchReminder(id, '/recur', recurrence);
-}
-
-function patchReminder(id, field, value) {
+export function getFirstReminder(subscriberId) {
     return new Promise((resolve, reject) => {
-        var data = [{
-            "op": "replace",
-            "path": field,
-            "value": value
-        }];
-        var patchHeaders = {
-            headers: {
-                'Content-Type': 'application/json-patch+json'
+        getList({
+            path: 'api/reminders/',
+            root: '_embedded.ngcp:reminders',
+            params: {
+                page: 1,
+                rows: 1,
+                subscriber_id: subscriberId
             }
-        };
-        Vue.http.patch('api/reminders/' + id, data, patchHeaders).then(() => {
-            resolve();
-        }).catch((err) => {
+        }).then((reminders)=>{
+            resolve(_.get(reminders, 'items.0', null));
+        }).catch((err)=>{
             reject(err);
         });
     });
 }
+
+export function getReminder(subscriberId) {
+    return new Promise((resolve, reject)=>{
+        Promise.resolve().then(()=>{
+            return getFirstReminder(subscriberId);
+        }).then((reminder)=>{
+            if(reminder === null) {
+                return createReminder(subscriberId);
+            }
+            else {
+                return Promise.resolve(reminder);
+            }
+        }).then((reminder)=>{
+            if(reminder === null) {
+                return getFirstReminder(subscriberId);
+            }
+            else {
+                return Promise.resolve(reminder);
+            }
+        }).then((reminder)=>{
+            resolve(reminder);
+        }).catch((err)=>{
+            reject(err);
+        });
+    });
+}
+
+export function setReminderActive(reminderId, active) {
+    return patchReplace({
+        path: 'api/reminders/' + reminderId,
+        fieldPath: 'active',
+        value: active
+    });
+}
+
+export function setReminderTime(reminderId, time) {
+    return patchReplace({
+        path: 'api/reminders/' + reminderId,
+        fieldPath: 'time',
+        value: time
+    });
+}
+
+export function setReminderRecurrence(reminderId, reccurence) {
+    return patchReplace({
+        path: 'api/reminders/' + reminderId,
+        fieldPath: 'recur',
+        value: reccurence
+    });
+}
+
+// export function enableReminder(id) {
+//     return patchReminder(id, '/active', true);
+// }
+//
+// export function disableReminder(id) {
+//     return patchReminder(id, '/active', false);
+// }
+//
+// export function setTime(id, time) {
+//     return patchReminder(id, '/time', time);
+// }
+//
+// export function setRecurrence(id, recurrence) {
+//     return patchReminder(id, '/recur', recurrence);
+// }
+//
+// function patchReminder(id, field, value) {
+//     return new Promise((resolve, reject) => {
+//         var data = [{
+//             "op": "replace",
+//             "path": field,
+//             "value": value
+//         }];
+//         var patchHeaders = {
+//             headers: {
+//                 'Content-Type': 'application/json-patch+json'
+//             }
+//         };
+//         Vue.http.patch('api/reminders/' + id, data, patchHeaders).then(() => {
+//             resolve();
+//         }).catch((err) => {
+//             reject(err);
+//         });
+//     });
+// }
