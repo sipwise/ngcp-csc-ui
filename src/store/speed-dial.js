1f2c57ef96678216ddef14aa49ca4a80745c3ae2
raxelsen <raxelsen@sipwise.com>
2018-07-23 16:10:25 +0200

    TT#40255 Customer wants to add speed dial config
    What has been done:
    - TT#40529, Implement API method for assigning a destination to a specific speed dial slot
    - TT#40630, Implement API method for fetching available slots
    - TT#40530, Implement reload of list after new assignment has been created successfully
    - TT#40527, Implement UI form and buttons (destination input field, slot selection field, and add button)
    - TT#40528, Implement custom phone number input field with automated formatting
    - TT#40531, Implement toast for successfully assigned destination
    - TT#40538, Implement unit test for creation of selections options for speed dial slot selection
    
    Change-Id: Ibe4937a097be0a89e677916a189a0e3fe62826df

--- src/store/speed-dial.js
+++ src/store/speed-dial.js
@@ -3,20 +3,25 @@
 import { i18n } from '../i18n';
 import { RequestState } from './common'
 import {
-    getSpeedDials,
-    unassignSpeedDialSlot
+    getSpeedDialsById,
+    unassignSpeedDialSlot,
+    getUnassignedSlots,
+    assignSpeedDialSlot
 } from '../api/speed-dial';
 
 export default {
     namespaced: true,
     state: {
         assignedSlots: [],
-        slotOptions: [],
         speedDialLoadingState: RequestState.initiated,
         speedDialError: null,
         unassignSlotState: RequestState.initiated,
         unassignSlotError: null,
-        lastUnassignedSlot: null
+        lastUnassignedSlot: null,
+        unassignedSlots: [],
+        assignSlotState: RequestState.initiated,
+        assignSlotError: null,
+        lastAssignedSlot: null
     },
     getters: {
         reminderLoadingState(state) {
@@ -45,6 +50,21 @@ export default {
         },
         lastUnassignedSlot(state) {
             return state.lastUnassignedSlot;
+        },
+        unassignedSlots(state) {
+            return state.unassignedSlots;
+        },
+        assignSlotState(state) {
+            return state.assignSlotState;
+        },
+        assignSlotError(state) {
+            return state.assignSlotError || i18n.t('speedDial.assignSlotErrorMessage');
+        },
+        lastAssignedSlot(state) {
+            return state.lastAssignedSlot;
+        },
+        isAdding(state) {
+            return state.assignSlotState === RequestState.requesting;
         }
     },
     mutations: {
@@ -73,17 +93,34 @@ export default {
         unassignSlotFailed(state, error) {
             state.unassignSlotState = RequestState.failed;
             state.unassignSlotError = error;
-        }
+        },
+        loadUnassignedSlots(state, result) {
+            state.unassignedSlots = result;
+        },
+        assignSlotRequesting(state) {
+            state.assignSlotState = RequestState.requesting;
+            state.assignSlotError = null;
+        },
+        assignSlotSucceeded(state, last) {
+            state.lastAssignedSlot = last;
+            state.assignSlotState = RequestState.succeeded;
+            state.assignSlotError = null;
+        },
+        assignSlotFailed(state, error) {
+            state.assignSlotState = RequestState.failed;
+            state.assignSlotError = error;
+        },
     },
     actions: {
         loadSpeedDials(context) {
             context.commit('speedDialRequesting');
-            getSpeedDials(context.getters.subscriberId).then((slots) => {
+            getSpeedDialsById(context.getters.subscriberId).then((slots) => {
                 context.commit('speedDialSucceeded', slots);
             }).catch((error) => {
                 context.commit('speedDialFailed', error);
             });
         },
+        // TODO: Check if you can rename slot param to assignment or config
         unassignSpeedDialSlot(context, slot) {
             context.commit('unassignSlotRequesting');
             unassignSpeedDialSlot({
@@ -95,6 +132,26 @@ export default {
                 context.dispatch('loadSpeedDials');
             }).catch((error) => {
                 context.commit('unassignSlotFailed', error);
+            });
+        },
+        getUnassignedSlots(context) {
+            getUnassignedSlots(context.getters.subscriberId).then((result) => {
+                context.commit('loadUnassignedSlots', result);
+            });
+        },
+        // TODO: Check if you can rename slot param to assignment or config
+        assignSpeedDialSlot(context, slot) {
+            context.commit('assignSlotRequesting');
+            assignSpeedDialSlot({
+                id: context.getters.subscriberId,
+                slot: slot
+            }).then(() => {
+                // TODO: Group into one succeeded mutation
+                context.commit('assignSlotSucceeded', slot.slot);
+                context.dispatch('loadSpeedDials');
+                context.dispatch('getUnassignedSlots');
+            }).catch((error) => {
+                context.commit('assignSlotFailed', error);
             });
         }
     }
--- t/api/speed-dial.js
+++ t/api/speed-dial.js
@@ -7,13 +7,14 @@ import {
     getFieldList
 } from '../../src/api/common';
 import {
-    getSpeedDials
+    getSpeedDialsById,
+    getUnassignedSlots
 } from '../../src/api/speed-dial';
 import { assert } from 'chai';
 
 Vue.use(VueResource);
 
-describe('Speed Dials', function(){
+describe('SpeedDial', function(){
 
     const subscriberId = 123;
 
@@ -88,8 +89,103 @@ describe('Speed Dials', function(){
                 status: 200
             }));
         });
-        getSpeedDials(subscriberId).then((result)=>{
+        getSpeedDialsById(subscriberId).then((result)=>{
             assert.deepEqual(result, fieldList);
+            done();
+        }).catch((err)=>{
+            done(err);
+        });
+    });
+
+    it('should get list of unassigned speed dial slots', function(done){
+
+        let data = {
+            "_links" : {
+                "collection" : {
+                    "href" : "/api/speeddials/"
+                },
+                "curies" : {
+                    "href" : "http://purl.org/sipwise/ngcp-api/#rel-{rel}",
+                    "name" : "ngcp",
+                    "templated" : true
+                },
+                "ngcp:journal" : [
+                    {
+                        "href" : "/api/speeddials/323/journal/"
+                    }
+                ],
+                "ngcp:speeddials" : [
+                    {
+                        "href" : "/api/speeddials/323"
+                    }
+                ],
+                "ngcp:subscribers" : [
+                    {
+                        "href" : "/api/subscribers/323"
+                    }
+                ],
+                "profile" : {
+                    "href" : "http://purl.org/sipwise/ngcp-api/"
+                },
+                "self" : {
+                    "href" : "/api/speeddials/323"
+                }
+            },
+            "speeddials" : [
+                {
+                    "destination" : "sip:439965050@10.15.17.240",
+                    "slot" : "*9"
+                },
+                {
+                    "destination" : "sip:22222222@10.15.17.240",
+                    "slot" : "*0"
+                },
+                {
+                    "destination" : "sip:43665522@10.15.17.240",
+                    "slot" : "*3"
+                }
+            ]
+        };
+
+        let slotOptions = [
+                {
+                    "label" : "Slot *1",
+                    "value" : "*1"
+                },
+                {
+                    "label" : "Slot *2",
+                    "value" : "*2"
+                },
+                {
+                    "label" : "Slot *4",
+                    "value" : "*4"
+                },
+                {
+                    "label" : "Slot *5",
+                    "value" : "*5"
+                },
+                {
+                    "label" : "Slot *6",
+                    "value" : "*6"
+                },
+                {
+                    "label" : "Slot *7",
+                    "value" : "*7"
+                },
+                {
+                    "label" : "Slot *8",
+                    "value" : "*8"
+                }
+        ];
+
+        Vue.http.interceptors = [];
+        Vue.http.interceptors.unshift((request, next)=>{
+            next(request.respondWith(JSON.stringify(data), {
+                status: 200
+            }));
+        });
+        getUnassignedSlots(subscriberId).then((result)=>{
+            assert.deepEqual(result, slotOptions);
             done();
         }).catch((err)=>{
             done(err);
