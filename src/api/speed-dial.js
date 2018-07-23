a0f22088afba473354b3d957658250cefaff1146
raxelsen <raxelsen@sipwise.com>
2018-07-23 16:10:25 +0200

    TT#40255 Customer wants to add speed dial config
    What has been done:
    - TT#40529, Implement API method for assigning a destination to a specific speed dial slot
    - TT#40630, Implement API method for fetching available slots
    - TT#40530, Implement reload of list after new assignment has been created successfully
    - TT#40527, Implement UI form and buttons (destination input field, slot selection field, and add button)
    - TT#40528, Implement custom phone number input field with automated formatting
    - TT#40531, Implement toast for successfully assigned destination
    
    What is remaining:
    - TT#40538, Implement unit test for creation of selections options for speed dial slot selection
    
    Change-Id: Ibe4937a097be0a89e677916a189a0e3fe62826df

--- src/api/speed-dial.js
+++ src/api/speed-dial.js
@@ -1,9 +1,10 @@
 
 import _ from 'lodash'
 import Vue from 'vue';
+import { i18n } from '../i18n';
 import { getFieldList } from './common'
 
-export function getSpeedDials(id) {
+export function getSpeedDialsById(id) {
     return new Promise((resolve, reject) => {
         getFieldList({
             path: 'api/speeddials/' + id,
@@ -21,7 +22,7 @@ export function getUnassignedSlots(id) {
     return new Promise((resolve, reject) => {
         let slots = ["*0", "*1", "*2", "*3", "*4", "*5", "*6", "*7", "*8", "*9"];
         Promise.resolve().then(() => {
-            return getSpeedDials(id);
+            return getSpeedDialsById(id);
         }).then((assignedSlots) => {
             // TODO: Split into own testable function that takes slots and
             // unassigned slots, and outputs slotOptions array ready to be
@@ -29,7 +30,14 @@ export function getUnassignedSlots(id) {
             let unassignedSlots = _.difference(slots, assignedSlots.map((slot) => {
                 return slot.slot;
             }));
-            resolve(unassignedSlots);
+            let slotOptions = [];
+            unassignedSlots.forEach((slot) => {
+                slotOptions.push({
+                    label: `${i18n.t('speedDial.slot')} ${slot}`,
+                    value: slot
+                });
+            });
+            resolve(slotOptions);
         }).catch((err) => {
             reject(err.body.message);
         });
@@ -50,6 +58,38 @@ export function unassignSpeedDialSlot(options) {
             resolve();
         }).catch((err) => {
             reject(err.body.message);
+        });
+    });
+}
+
+export function addSlotToSpeedDials(options) {
+    return new Promise((resolve, reject) => {
+        let headers = {
+            'Content-Type': 'application/json-patch+json'
+        };
+        Vue.http.patch('api/speeddials/' + options.id, [{
+            op: 'replace',
+            path: '/speeddials',
+            value: options.slots
+        }], { headers: headers }).then(() => {
+            resolve();
+        }).catch((err) => {
+            reject(err.body.message);
+        });
+    });
+}
+
+export function assignSpeedDialSlot(options) {
+    return new Promise((resolve, reject) => {
+        Promise.resolve().then(() => {
+            return getSpeedDialsById(options.id);
+        }).then((result) => {
+            let concatSlots = result.concat(options.slot);
+            return addSlotToSpeedDials({ id: options.id, slots: concatSlots });
+        }).then(() => {
+            resolve();
+        }).catch((err) => {
+            reject(err);
         });
     });
 }
--- src/components/pages/SpeedDial/SpeedDial.vue
+++ src/components/pages/SpeedDial/SpeedDial.vue
@@ -2,10 +2,20 @@
     <csc-page class="csc-list-page">
         <q-list
             no-border
-            inset-separator
+            separator
             sparse
             multiline
         >
+            <q-item>
+                <q-item-main>
+                    <csc-speed-dial-add-form
+                        ref="addForm"
+                        @save="addSpeedDial"
+                        :loading="isAdding"
+                        :slot-options="unassignedSlots"
+                    />
+                </q-item-main>
+            </q-item>
             <q-item
                 v-for="(assigned, index) in assignedSlots"
                 :key="index"
@@ -53,13 +63,14 @@
 
 <script>
     import { mapGetters } from 'vuex'
+    import CscPage from '../../CscPage'
+    import CscSpeedDialAddForm from './CscSpeedDialAddForm'
     import {
         startLoading,
         stopLoading,
         showToast,
         showGlobalError
-    } from '../../helpers/ui'
-    import CscPage from '../CscPage'
+    } from '../../../helpers/ui'
     import {
         QList,
         QItem,
@@ -72,8 +83,14 @@
     } from 'quasar-framework'
 
     export default {
+        data () {
+            return {
+                addFormEnabled: true
+            }
+        },
         components: {
             CscPage,
+            CscSpeedDialAddForm,
             QList,
             QItem,
             QItemMain,
@@ -84,6 +101,7 @@
         },
         created() {
             this.$store.dispatch('speedDial/loadSpeedDials');
+            this.$store.dispatch('speedDial/getUnassignedSlots');
         },
         computed: {
             ...mapGetters('speedDial', [
@@ -92,10 +110,18 @@
                 'speedDialLoadingError',
                 'unassignSlotState',
                 'unassignSlotError',
-                'lastUnassignedSlot'
+                'lastUnassignedSlot',
+                'unassignedSlots',
+                'assignSlotState',
+                'assignSlotError',
+                'lastAssignedSlot',
+                'isAdding'
             ])
         },
         methods: {
+            addSpeedDial(slot) {
+                this.$store.dispatch('speedDial/assignSpeedDialSlot', slot);
+            },
             unassignSlot(slot) {
                 let self = this;
                 let store = this.$store;
@@ -142,6 +168,17 @@
                     stopLoading();
                     showToast(this.$t('speedDial.unassignSlotSuccessMessage', {
                         slot: this.lastUnassignedSlot
+                    }));
+                }
+            },
+            assignSlotState(state) {
+                if (state === 'failed') {
+                    showGlobalError(this.assignSlotError);
+                }
+                else if (state === 'succeeded') {
+                    this.$refs.addForm.cancel();
+                    showToast(this.$t('speedDial.assignSlotSuccessMessage', {
+                        slot: this.lastAssignedSlot
                     }));
                 }
             }
