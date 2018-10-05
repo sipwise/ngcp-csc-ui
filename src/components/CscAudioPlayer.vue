a61934343977e7ffd0f616cdbc07b94b1c911550
raxelsen <raxelsen@sipwise.com>
2018-10-05 15:56:26 +0200

    TT#44661 Upload a custom "busy" greeting sound
    What has been done:
    - TT#44661 Voicebox: As a Customer, I want to upload a custom "busy"
      greeting sound
    - TT#44663, Voicebox: As a Customer, I want to delete/reset the custom
      "busy" greeting sound
    - Refactoring of CscVoiceMailPlayer to more generic CscAudioPlayer
    
    Change-Id: I24f9d607b9d1acece56cd47c0bd8c99d125d45b4

--- src/components/CscAudioPlayer.vue
+++ src/components/CscAudioPlayer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="voicemail-player">
-        <audio :src="soundFileUrl" ref="voiceMailSound" preload="auto" @timeupdate="timeupdate($event)"/>
+        <audio :src="fileUrl" ref="audio" preload="auto" @timeupdate="timeupdate($event)"/>
         <div class="control-btns">
             <q-btn class="play-pause-btn" round flat small color="primary"
                    :icon="playPauseIcon" @click="toggle()" />
@@ -12,28 +12,28 @@
 </template>
 
 <script>
-    import { mapGetters } from 'vuex'
     import { QProgress, QBtn } from 'quasar-framework'
 
     export default {
-        name: 'csc-voice-mail-player',
-        props: {
-            id: Number
-        },
+        name: 'csc-audio-player',
+        props: [
+           'fileUrl',
+           'loaded'
+        ],
         mounted() {
-            this.$refs.voiceMailSound.addEventListener('play', ()=>{
+            this.$refs.audio.addEventListener('play', ()=>{
                 this.playing = true;
             });
-            this.$refs.voiceMailSound.addEventListener('playing', ()=>{
+            this.$refs.audio.addEventListener('playing', ()=>{
                 this.playing = true;
             });
-            this.$refs.voiceMailSound.addEventListener('ended', ()=>{
+            this.$refs.audio.addEventListener('ended', ()=>{
                 this.playing = false;
                 this.stop();
             });
-            this.$refs.voiceMailSound.addEventListener('canplay', ()=>{
+            this.$refs.audio.addEventListener('canplay', ()=>{
                 if(!this.paused && this.playing) {
-                    this.$refs.voiceMailSound.play();
+                    this.$refs.audio.play();
                 }
             });
         },
@@ -43,7 +43,6 @@
         },
         data () {
             return {
-                platform: this.$q.platform.is,
                 playing: false,
                 paused: false,
                 progressPercentage: 0
@@ -52,63 +51,53 @@
         computed: {
             playPauseIcon() {
                 return this.playing ? 'pause': 'play_arrow';
-            },
-            soundFileFormat() {
-                return this.platform.mozilla ? 'ogg' : 'mp3';
-            },
-            soundFileUrl() {
-                let getter = this.playVoiceMailUrl;
-                return getter(this.id);
-            },
-            ...mapGetters('conversations', [
-                'playVoiceMailState',
-                'playVoiceMailUrl'
-            ])
+            }
         },
         methods: {
             play() {
-                this.$refs.voiceMailSound.play();
+                this.$refs.audio.play();
                 this.playing = true;
                 this.paused = false;
             },
             pause() {
-                this.$refs.voiceMailSound.pause();
+                this.$refs.audio.pause();
                 this.playing = false;
                 this.paused = true;
             },
             stop() {
-                this.$refs.voiceMailSound.currentTime = 0;
+                this.$refs.audio.currentTime = 0;
                 this.pause();
             },
-            load() {
-                this.$emit('play-voice-mail', {
-                    id: this.id,
-                    format: this.soundFileFormat
-                });
+            setPlayingTrue() {
                 this.playing = true;
+            },
+            setPausedFalse() {
                 this.paused = false;
             },
+            timeupdate(e) {
+                let newPercentage = Math.floor((e.target.currentTime / e.target.duration) * 100);
+                this.progressPercentage = newPercentage;
+            },
+            load() {
+                this.$emit('load');
+            },
             toggle() {
-                if(this.playVoiceMailState(this.id) !== 'succeeded') {
+                if (!this.loaded) {
                     this.load();
                 }
-                else if (this.$refs.voiceMailSound.paused) {
+                else if (this.$refs.audio.paused) {
                     this.play();
                 }
                 else {
                     this.pause();
                 }
-            },
-            timeupdate(e) {
-                let newPercentage = Math.floor((e.target.currentTime / e.target.duration) * 100);
-                this.progressPercentage = newPercentage;
             }
         }
     }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-    @import '../../../themes/app.common'
+    @import '../themes/app.common'
 
     .voicemail-player
         width 100%
