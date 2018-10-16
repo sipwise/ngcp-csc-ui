2fb46919430a336860b8488f23c06513be54732d
Hans-Peter Herzog <hherzog@sipwise.com>
2018-10-16 12:48:38 +0200

    TT#45686 Development: Extend dev config script to enable conferencing and related services by default
    Change-Id: I30e057b514997c57d96e4269f0e9e93d87b241f3

--- dev-config.sh
+++ dev-config.sh
@@ -8,6 +8,13 @@ case "$1" in
     ngcpcfg set /etc/ngcp-config/config.yml pbx.enable=yes
     ngcpcfg set /etc/ngcp-config/config.yml www_admin.privileges.subscriberadmin.subscribers.0=write
     ngcpcfg set /etc/ngcp-config/config.yml email.smarthost.hostname=mail.sipwise.com
+
+    # Mandatory flags to enable conferencing
+    ngcpcfg set /etc/ngcp-config/config.yml rtcengine.conference.enable=yes
+    ngcpcfg set /etc/ngcp-config/config.yml janus.enable=yes
+    ngcpcfg set /etc/ngcp-config/config.yml fileshare.enable=yes
+
+    # Apply changes
     ngcpcfg apply 'Enable CSC, PBX and RtcEngine'
     ;;
 esac
