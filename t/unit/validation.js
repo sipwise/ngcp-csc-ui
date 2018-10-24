87dab64d7726bd7dbe093ff7a5cd73200c0dc30e
raxelsen <raxelsen@sipwise.com>
2018-10-24 11:51:50 +0200

    TT#46168 Impl MAC Address validation in PBXDevices
    Change-Id: I5bff71547fb9417541d5dae37aaa4230dd517a14

--- t/unit/validation.js
+++ t/unit/validation.js
@@ -3,6 +3,7 @@
 
 import { assert } from 'chai';
 import { userInfo } from '../../src/helpers/validation'
+import { customMacAddress } from '../../src/helpers/validation'
 
 describe('Userinfo validation helper', function() {
 
@@ -28,3 +29,31 @@ describe('Userinfo validation helper', function() {
 
 });
 
+describe('Custom mac address validation helper', function() {
+
+    it('should validate mac address separated by colon', function() {
+        let input = "13:14:5f:cD:42:5f";
+        assert.isTrue(customMacAddress(input));
+    });
+
+    it('should validate mac address separated by hyphen', function() {
+        let input = "13-14-5f-cD-42-5f";
+        assert.isTrue(customMacAddress(input));
+    });
+
+    it('should validate mac address without separator', function() {
+        let input = "13145fcD425f";
+        assert.isTrue(customMacAddress(input));
+    });
+
+    it('should not validate mac address with mixed separator', function() {
+        let input = "13:14:5f:cD:42-5f";
+        assert.isFalse(customMacAddress(input));
+    });
+
+    it('should not validate mac address when invalid', function() {
+        let input = "k183p1r23411";
+        assert.isFalse(customMacAddress(input));
+    });
+
+});
