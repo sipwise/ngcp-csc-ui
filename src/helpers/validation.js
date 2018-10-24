9e4ad2d355e78a9f18a72025ba6d2455dc6f6621
raxelsen <raxelsen@sipwise.com>
2018-10-24 11:51:50 +0200

    TT#46168 Impl MAC Address validation in PBXDevices
    Change-Id: I5bff71547fb9417541d5dae37aaa4230dd517a14

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -3,3 +3,8 @@ export function userInfo (value) {
     var regex = new RegExp(/^[-_.!~*'&=+$,;?/%a-zA-Z0-9]+$/);
     return regex.test(value);
 }
+
+export function customMacAddress (value) {
+    var regex = new RegExp(/^(?:(?:[0-9A-Fa-f]{2}(?=([-:]|))(?:\1[0-9A-Fa-f]{2}){5}))$/);
+    return regex.test(value);
+}
