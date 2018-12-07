04ad67f7feeab067177eb2cb71edbc352f2fd210
raxelsen <raxelsen@sipwise.com>
2018-12-07 14:43:22 +0100

    TT#47986 Jump from a Seat/Group to CallQueue item
    What has been done:
    - TT#47986, Implement UI button for jumping from Seat/Group item to
      CallQueueConfiguration item
    - TT#47987, Implement router link which passes parameter to
      CallQueueConfiguration component
    - TT#47988, Implement highlighting of CallQueueConfiguration item upon
      route change
    - TT#47989, Implement "scroll to item" capability in
      CallQueueConfiguration component
    - TT#48594, Extend Seats/Groups list API method to also fetch call queue
      preference needed
    
    Change-Id: I31797e12889d1c5bd32b330550cec04e67edee93

--- src/components/pages/PbxConfiguration/CscPbxCallQueue.vue
+++ src/components/pages/PbxConfiguration/CscPbxCallQueue.vue
@@ -3,96 +3,97 @@
     <q-item
         :class="itemClasses"
     >
-        <q-item-side
-            v-if="!expanded"
-        >
-            <q-icon
-                size="24px"
-                name="queue"
-                color="white"
-            />
-        </q-item-side>
-        <q-item-main>
-            <q-item-tile
-                v-if="!expanded"
-                class="csc-item-title"
-                label
-            >
-                <q-icon
-                    v-if="subscriber.is_pbx_group"
-                    size="24px"
-                    name="group"
-                    color="white"
-                />
-                <q-icon
-                    v-else
-                    size="24px"
-                    name="person"
-                    color="white"
-                />
-                <span class="csc-item-label">{{ subscriber.display_name }}</span>
-            </q-item-tile>
-            <q-item-tile
-                v-if="!expanded"
-                class="csc-item-subtitle"
-                sublabel
-            >
-                <span class="csc-item-label">{{ $t('pbxConfig.queueLength') }}:</span>
-                <span class="csc-item-value">{{ subscriber.max_queue_length }}</span>
-            </q-item-tile>
-            <q-item-tile
-                v-if="!expanded"
-                class="csc-item-subtitle"
-                sublabel
-            >
-                <span class="csc-item-label">{{ $t('pbxConfig.wrapUpTime') }}:</span>
-                <span class="csc-item-value">{{ subscriber.queue_wrap_up_time }}</span>
-            </q-item-tile>
-            <q-item-tile
-                class="csc-list-item-main"
-                v-if="expanded"
-            >
-                <q-field
-                    :label="$t('pbxConfig.queueExtensionName')">
-                    <q-input
-                        dark
-                        readonly
-                        :value="subscriber.display_name"
-                    />
-                </q-field>
-                <q-field
-                    :label="$t('pbxConfig.queueLength')">
-                    <q-input
-                        dark
-                        readonly
-                        :value="subscriber.max_queue_length"
-                    />
-                </q-field>
-                <q-field
-                    :label="$t('pbxConfig.wrapUpTime')">
-                    <q-input
-                        dark
-                        readonly
-                        :value="subscriber.queue_wrap_up_time"
-                        suffix="seconds"
-                    />
-                </q-field>
-            </q-item-tile>
-        </q-item-main>
-        <q-item-side
-            right
-            class="csc-list-actions-pinned"
-        >
-            <q-item-tile>
-                <q-btn
-                    :icon="titleIcon"
-                    :big="isMobile"
-                    color="primary"
-                    flat
-                    @click="toggleMain()"
-                />
-            </q-item-tile>
-        </q-item-side>
+		<q-item-side
+			v-if="!expanded"
+		>
+			<q-icon
+				size="24px"
+				name="queue"
+				color="white"
+			/>
+		</q-item-side>
+		<q-item-main>
+			<q-item-tile
+				v-if="!expanded"
+				class="csc-item-title"
+				label
+			>
+				<q-icon
+					v-if="subscriber.is_pbx_group"
+					size="24px"
+					name="group"
+					color="white"
+				/>
+				<q-icon
+					v-else
+					size="24px"
+					name="person"
+					color="white"
+				/>
+				<span class="csc-item-label">{{ subscriber.display_name }}</span>
+			</q-item-tile>
+			<q-item-tile
+				v-if="!expanded"
+				class="csc-item-subtitle"
+				sublabel
+			>
+				<span class="csc-item-label">{{ $t('pbxConfig.queueLength') }}:</span>
+				<span class="csc-item-value">{{ subscriber.max_queue_length }}</span>
+			</q-item-tile>
+			<q-item-tile
+				v-if="!expanded"
+				class="csc-item-subtitle"
+				sublabel
+			>
+				<span class="csc-item-label">{{ $t('pbxConfig.wrapUpTime') }}:</span>
+				<span class="csc-item-value">{{ subscriber.queue_wrap_up_time }}</span>
+			</q-item-tile>
+			<q-item-tile
+				class="csc-list-item-main"
+				v-if="expanded"
+			>
+				<q-field
+					:label="$t('pbxConfig.queueExtensionName')">
+					<q-input
+						dark
+						readonly
+						:value="subscriber.display_name"
+					/>
+				</q-field>
+				<q-field
+					:label="$t('pbxConfig.queueLength')">
+					<q-input
+						dark
+						readonly
+						:value="subscriber.max_queue_length"
+					/>
+				</q-field>
+				<q-field
+					:label="$t('pbxConfig.wrapUpTime')">
+					<q-input
+						dark
+						readonly
+						:value="subscriber.queue_wrap_up_time"
+						suffix="seconds"
+					/>
+				</q-field>
+			</q-item-tile>
+		</q-item-main>
+		<q-item-side
+			right
+			class="csc-list-actions-pinned"
+			key="queue-side"
+		>
+			<q-item-tile>
+				<q-btn
+					:icon="titleIcon"
+					:big="isMobile"
+					:color="caretColor"
+					flat
+					@click="toggleMain()"
+				/>
+			</q-item-tile>
+		</q-item-side>
     </q-item>
 </template>
 
@@ -111,11 +112,13 @@
     export default {
         name: 'csc-pbx-call-queue',
         props: [
-            'subscriber'
+            'subscriber',
+            'highlight'
         ],
         data () {
             return {
-                expanded: false
+                expanded: this.highlight,
+                highlightCollapsed: false
             }
         },
         components: {
@@ -129,9 +132,19 @@
             QItemTile
         },
         computed: {
+			customName() {
+				return this.highlight ? 'custom' : null;
+			},
+            highlighted() {
+                return this.expanded && this.highlight && !this.highlightCollapsed;
+            },
             itemClasses() {
                 let classes = ['csc-list-item', 'csc-pbx-call-queue'];
-                if (this.expanded) {
+                if (this.highlighted) {
+                    classes.push('csc-item-expanded');
+                    classes.push('csc-item-highlight');
+                }
+                else if (this.expanded) {
                     classes.push('csc-item-expanded');
                 }
                 else {
@@ -149,12 +162,16 @@
                 else {
                     return 'keyboard arrow up';
                 }
+            },
+            caretColor() {
+                return this.highlighted ? 'white' : 'primary';
             }
         },
         methods: {
             toggleMain() {
+                this.highlightCollapsed = true;
                 this.expanded = !this.expanded;
-            }
+			}
         },
         watch: {
         }
