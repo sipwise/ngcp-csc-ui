5fb9588ed79edd2540997ef5d14be44b8b9e3683
raxelsen <raxelsen@sipwise.com>
2019-01-23 15:23:26 +0100

    TT#47510 Extra page section for managing SoundSets
    What has been done:
    - TT#48188, Add new menu entry and route for SoundManagement
    - TT#48189, Create component for SoundManagement page
    
    Change-Id: I1e1d6d49d90d1b8b5deef5689c686b921eb6f7d8

---
+++ src/components/pages/PbxConfiguration/CscPbxSoundSets.vue
@@ -0,0 +1,39 @@
+<template>
+    <csc-page
+        :is-list="true"
+    >
+    </csc-page>
+</template>
+
+<script>
+    import CscPage from '../../CscPage'
+    //import { mapGetters } from 'vuex'
+    //import {
+    //    QList,
+    //    QItem
+    //} from 'quasar-framework'
+    export default {
+        components: {
+            CscPage,
+        },
+        data () {
+            return {
+            }
+        },
+        mounted() {
+        },
+        computed: {
+            //...mapGetters('pbxConfig', [
+            //    'placeholder'
+            //]),
+        },
+        methods: {
+        },
+        watch: {
+        }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    @import '../../../themes/quasar.variables.styl';
+</style>
