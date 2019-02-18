b4e741705b01bde6260210c0d3e6a6beb8e89517
raxelsen <raxelsen@sipwise.com>
2019-02-18 16:47:05 +0100

    TT#53365 Customer wants extra user settings page
    What has been done:
    - TT#53365, General: As a Customer, I want to see an extra page for the
      user settings
      - TT#53400, Add UI customer-menu entry for User Settings
      - TT#53316, Create component for User Settings page
      - TT#53401, Create route for User Settings
    
    Change-Id: I3c3a1d386664113055ccac9dae9eff9fe7a6689d

---
+++ src/components/pages/UserSettings.vue
@@ -0,0 +1,77 @@
+
+<template>
+    <csc-page class="csc-simple-page">
+        <!--<div-->
+            <!--class="row">-->
+            <!--<div-->
+                <!--class="col col-xs-12 col-md-6">-->
+                <!--<q-field-->
+                    <!--class="csc-form-field"-->
+                    <!--icon="language">-->
+                    <!--<q-inner-loading :visible="false">-->
+                        <!--<q-spinner-dots-->
+                            <!--color="primary"-->
+                            <!--:size="24"/>-->
+                    <!--</q-inner-loading>-->
+                    <!--<q-select-->
+                        <!--dark-->
+                        <!--:disable="false"-->
+                        <!--:float-label="$t('userSettings.changeLanguage')"-->
+                        <!--v-model="language"-->
+                        <!--:options="languageOptions"-->
+                        <!--@change="updateLanguage"/>-->
+                <!--</q-field>-->
+            <!--</div>-->
+        <!--</div>-->
+    </csc-page>
+</template>
+
+<script>
+    //import { mapGetters } from 'vuex'
+    //import {
+    //    startLoading,
+    //    stopLoading,
+    //    showToast,
+    //} from '../../helpers/ui'
+    //import {
+    //    QField,
+    //    QSelect,
+    //    QInnerLoading,
+    //    QSpinnerDots,
+    //    QBtn
+    //} from 'quasar-framework'
+    import CscPage from '../CscPage'
+
+    export default {
+        data () {
+            return {
+                language: 'en',
+                languageOptions: [
+                    {
+                        label: 'English',
+                        value: 'en'
+                    },
+                    {
+                        label: 'Français',
+                        value: 'fr'
+                    }
+                ]
+            }
+        },
+        components: {
+            CscPage
+        },
+        created() {
+        },
+        computed: {
+        },
+        methods: {
+        },
+        watch: {
+        }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    @import '../../themes/quasar.variables.styl';
+</style>
