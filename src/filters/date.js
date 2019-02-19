fa028970e836f8b2d6733df8bb5519bba0d1c2f8
raxelsen <raxelsen@sipwise.com>
2019-02-19 12:56:44 +0100

    TT#52974 Call history shows wrong call log time
    Change-Id: I78de755a2c7ffb4b3fe627af41ced3bf7ad9cdb7
    (cherry picked from commit 769b2fdf4903b5f7e4d559d4e66c3489f523ce89)

--- src/filters/date.js
+++ src/filters/date.js
@@ -9,31 +9,31 @@ export default function(value) {
     return `${formatDate(timeStamp, 'MMMM D, YYYY')} at ${formatDate(timeStamp, 'h:mm a')}`;
 }
 
-export function smartTime($date, $today) {
+export function smartTime($date) {
 
-    let today = $today || new Date();
-    let date = moment($date, 'YYYY-MM-DD HH:mm:SS').toDate();
+    let today = new Date();
+    let date = moment.utc($date, 'YYYY-MM-DD HH:mm:SS').toDate();
     let diffSeconds = Math.floor((today.getTime() - date.getTime()) / 1000);
     let diffMinutes = Math.floor(diffSeconds / 60);
     let momentDate = moment(date);
 
     let seconds = 'second';
-    if(diffSeconds > 1) {
+    if (diffSeconds > 1) {
         seconds = seconds + "s";
     }
 
     let minutes = 'minute';
-    if(diffSeconds > 120) {
+    if (diffSeconds > 120) {
         minutes = minutes + "s";
     }
 
-    if(diffSeconds < 60) {
+    if (diffSeconds < 60) {
         return diffSeconds + ' ' + seconds + ' ago';
     }
     else if (diffSeconds < 3600) {
         return diffMinutes + ' ' + minutes + ' ago';
     }
-    else if(isToday(date)) {
+    else if (isToday(date)) {
         return 'Today, ' + momentDate.format('HH:mm');
     }
     else if (isYesterday(date)) {
