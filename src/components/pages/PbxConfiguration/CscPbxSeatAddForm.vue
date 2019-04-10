654d381766285458b5e7e1369430ae10ec93ee5e
Robert Axelsen <raxelsen@sipwise.com>
2019-04-10 11:59:09 +0200

    TT#55911 Assign SoundSet to PBXGroup or PBXSeat
    Change-Id: Ic89de6e03e1ced79c94f5ac107f3ad1560ea8b1d

--- src/components/pages/PbxConfiguration/CscPbxSeatAddForm.vue
+++ src/components/pages/PbxConfiguration/CscPbxSeatAddForm.vue
@@ -30,9 +30,9 @@
         <q-field>
             <q-select
                 dark
-                multiple
                 chips
                 clearable
+                multiple
                 v-model="data.aliasNumbers"
                 :disable="loading"
                 :readonly="loading"
@@ -43,15 +43,31 @@
         <q-field>
             <q-select
                 dark
-                multiple
                 chips
                 clearable
+                multiple
                 v-model="data.groups"
                 :disable="loading"
                 :readonly="loading"
                 :float-label="$t('pbxConfig.groups')"
                 :options="groupOptions"
             />
+        </q-field>
+        <q-field>
+            <q-select
+                dark
+                clearable
+                v-model="soundSet"
+                :disable="loading || !defaultSoundSet"
+                :readonly="loading"
+                :float-label="$t('pbxConfig.soundSet')"
+                :options="soundSetOptions"
+            />
+            <q-tooltip
+                v-if="!defaultSoundSet"
+            >
+                {{ $t('pbxConfig.defaultNotSet') }}
+            </q-tooltip>
         </q-field>
         <div class="csc-form-actions row justify-center">
             <q-btn
@@ -75,7 +91,7 @@
             </q-btn>
         </div>
         <q-inner-loading :visible="loading">
-            <q-spinner-mat size="60px" color="primary" />
+            <q-spinner-dots size="60px" color="primary" />
         </q-inner-loading>
     </div>
 </template>
@@ -89,11 +105,12 @@
     import {
         QBtn,
         QInnerLoading,
-        QSpinnerMat,
+        QSpinnerDots,
         QField,
         QInput,
         QSelect,
-        QIcon
+        QIcon,
+        QTooltip
     } from 'quasar-framework'
 
     export default {
@@ -101,16 +118,20 @@
         props: [
             'aliasNumberOptions',
             'groupOptions',
-            'loading'
+            'loading',
+            'soundSetOptions',
+            'soundSetValue',
+            'defaultSoundSet'
         ],
         components: {
             QBtn,
             QInnerLoading,
-            QSpinnerMat,
+            QSpinnerDots,
             QField,
             QInput,
             QSelect,
-            QIcon
+            QIcon,
+            QTooltip
         },
         validations: {
             data: {
@@ -127,7 +148,13 @@
         },
         data () {
             return {
-                data: this.getDefaults()
+                data: this.getDefaults(),
+                soundSet: null
+            }
+        },
+        created() {
+            if (this.defaultSoundSet) {
+                this.soundSet = this.defaultSoundSet;
             }
         },
         computed: {
@@ -165,6 +192,7 @@
         },
         methods: {
             getDefaults() {
+                // TODO: Add soundSet here, as in CscPbxSeat.vue etc
                 return {
                     name: '',
                     extension: '',
@@ -176,10 +204,11 @@
                 this.$emit('cancel');
             },
             save() {
-                this.$emit('save', this.data);
+                this.$emit('save', Object.assign({ soundSet: this.soundSetValue(this.soundSet) }, this.data));
             },
             reset() {
                 this.data = this.getDefaults();
+                this.soundSet = null;
                 this.$v.$reset();
             }
         }
--- src/components/pages/PbxConfiguration/CscPbxSeats.vue
+++ src/components/pages/PbxConfiguration/CscPbxSeats.vue
@@ -24,7 +24,10 @@
                 class="col-xs-12 col-md-6 csc-list-form"
                 :alias-number-options="aliasNumberOptions"
                 :group-options="groupOptions"
+                :sound-set-options="soundSetOptions"
+                :sound-set-value="soundSetValue"
                 :loading="isAdding"
+                :default-sound-set="!!defaultSoundSet"
                 @save="addSeat"
                 @cancel="disableAddForm"
             />
@@ -61,12 +64,16 @@
                     :seat="seat"
                     :alias-number-options="aliasNumberOptions"
                     :group-options="groupOptions"
-                    @remove="removeSeatDialog"
                     :loading="isItemLoading(seat.id)"
+                    :sound-set-options="soundSetOptions"
+                    :sound-set-value="soundSetValue"
+                    :default-sound-set="!!defaultSoundSet"
+                    @remove="removeSeatDialog"
                     @save-name="setSeatName"
                     @save-extension="setSeatExtension"
                     @save-alias-numbers="updateAliasNumbers"
                     @save-groups="updateGroups"
+                    @save-sound-set="updateSoundSet"
                 />
             </q-list>
         </div>
@@ -127,6 +134,8 @@
             this.$store.dispatch('pbxConfig/listSeats', {
                 page: 1
             });
+            this.$store.dispatch('pbxConfig/listSoundSets');
+            this.$store.dispatch('pbxConfig/getDefaultSoundSet');
         },
         data () {
             return {
@@ -185,19 +194,12 @@
                 'lastUpdatedField',
                 'updateAliasNumbersState',
                 'updateGroupsAndSeatsState',
-                'updateState'
+                'updateState',
+                'soundSetOptions',
+                'soundSetValue',
+                'defaultSoundSet',
+                'groupOptions'
             ]),
-            groupOptions() {
-                let groups = [];
-                this.groups.forEach((group)=>{
-                    groups.push({
-                        label: group.display_name ? group.display_name : group.username,
-                        sublabel: this.$t('pbxConfig.extension') + ': ' + group.pbx_extension,
-                        value: group.id
-                    });
-                });
-                return groups;
-            },
             isMobile() {
                 return Platform.is.mobile;
             },
@@ -272,6 +274,9 @@
             },
             updateGroups(data) {
                 this.$store.dispatch('pbxConfig/updateGroups', data);
+            },
+            updateSoundSet(data) {
+                this.$store.dispatch('pbxConfig/setSeatSoundSet', data);
             },
             changePage(page) {
                 this.$store.dispatch('pbxConfig/listSeats', {
