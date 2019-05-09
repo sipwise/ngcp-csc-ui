ef09f55fa52d9b96d741cafe16990bd9e1c96ac2
Robert Axelsen <raxelsen@sipwise.com>
2019-05-09 11:51:41 +0200

    TT#44075 List ManagerSecretaryConfigurations items
    What is remaining:
    - TT#56392, ManagerSecretary: As a PBXAdmin, I want to see a list of
    ManagerSecretaryConfigurations
    
    What has been done:
    - TT#44075, ManagerSecretary: As a PBXAdmin, I want to see a new section where
    I can manage ManagerSecretaryConfigurations
    
    Change-Id: I17638341420d0f456627fb4a214d5dc5b33bc44f

---
+++ src/components/pages/PbxConfiguration/CscPbxManagerSecretary.vue
@@ -0,0 +1,68 @@
+
+<template>
+    <csc-page
+        :is-list="true"
+    >
+        <!--<div-->
+            <!--v-if="isListLoadingVisible"-->
+            <!--class="row justify-center">-->
+            <!--<q-spinner-dots-->
+                <!--color="primary"-->
+                <!--:size="40"-->
+            <!--/>-->
+        <!--</div>-->
+        <!--<q-list-->
+            <!--striped-odd-->
+            <!--no-border-->
+            <!--multiline-->
+            <!--:highlight="!isMobile">-->
+            <!--<csc-pbx-manager-secretary-config />-->
+        <!--</q-list>-->
+        <!--<div-->
+            <!--v-if="managerSecretaryList.length === 0 && !isManagerSecretaryRequesting"-->
+            <!--class="row justify-center csc-no-entities">-->
+            <!--{{ $t('pbxConfig.noManagerSecretary') }}-->
+        <!--</div>-->
+    </csc-page>
+</template>
+
+<script>
+    import CscPage from '../../CscPage'
+    //import CscPbxManagerSecretaryConfig from './CscPbxManagerSecretaryConfig'
+    //import {
+    //    mapGetters
+    //} from 'vuex'
+    import {
+        Platform
+    } from 'quasar-framework'
+    export default {
+        components: {
+            CscPage
+        },
+        data () {
+            return {
+            }
+        },
+        mounted() {
+            //this.$store.dispatch('pbxConfig/listManagerSecretary');
+        },
+        computed: {
+            //...mapGetters('pbxConfig', [
+            //    'managerSecretaryList',
+            //    'isManagerSecretaryRequesting',
+            //    'isListLoadingVisible'
+            //]),
+            isMobile() {
+                return !!Platform.is.mobile;
+            }
+        },
+        methods: {
+        },
+        watch: {
+        }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    @import '../../../themes/quasar.variables.styl';
+</style>
