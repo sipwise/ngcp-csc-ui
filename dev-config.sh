3538e70c499f16116e2fb01c5dd2fe3ca90f674c
Hans-Peter Herzog <hherzog@sipwise.com>
2019-06-12 08:47:46 +0200

    PBX new
    Change-Id: If7a3e09fe73ca83f51a913aff73485f207b6f73f

--- dev-config.sh
+++ dev-config.sh
@@ -3,12 +3,6 @@
 case "$1" in
     *)
     ngcpcfg set /etc/ngcp-config/config.yml www_admin.http_csc.csc_dev=yes
-    ngcpcfg set /etc/ngcp-config/config.yml www_admin.http_csc.csc_js_enable=yes
-    ngcpcfg set /etc/ngcp-config/config.yml rtcengine.enable=yes
-    ngcpcfg set /etc/ngcp-config/config.yml rtcengine.conference.enable=yes
-    ngcpcfg set /etc/ngcp-config/config.yml rtcengine.conference.type=janus
-    ngcpcfg set /etc/ngcp-config/config.yml janus.enable=yes
-    ngcpcfg set /etc/ngcp-config/config.yml fileshare.enable=yes
     ngcpcfg set /etc/ngcp-config/config.yml pbx.enable=yes
     ngcpcfg set /etc/ngcp-config/config.yml www_admin.privileges.subscriberadmin.subscribers.0=write
     ngcpcfg apply 'Enable CSC, PBX and RtcEngine'
---
+++ src/components/CscListAddButton.vue
@@ -0,0 +1,45 @@
+<template>
+    <q-slide-transition>
+        <div
+            class="csc-add-button row justify-center"
+        >
+            <q-btn
+                color="primary"
+                icon="add"
+                flat
+                @click="click"
+            >
+                {{ label }}
+            </q-btn>
+        </div>
+    </q-slide-transition>
+</template>
+
+<script>
+    import {
+        QSlideTransition,
+        QBtn
+    } from 'quasar-framework'
+    export default {
+        name: 'csc-list-add-button',
+        data () {
+            return {}
+        },
+        props: [
+            'label'
+        ],
+        components: {
+            QSlideTransition,
+            QBtn
+        },
+        methods: {
+            click() {
+                this.$emit('click');
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    @import '../themes/quasar.variables.styl'
+</style>
