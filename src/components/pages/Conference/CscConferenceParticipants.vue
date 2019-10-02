37909a6bccd7c262ccefad9dd9781c56dc908478
Carlo Venusino <cvenusino@sipwise.com>
2019-10-02 16:43:11 +0200

    TT#47708 Conferencing > show participant local video
    Change-Id: I1ab7e1261673c27099f4f78cf6c8b5caf4531591

---
+++ src/components/pages/Conference/CscConferenceParticipants.vue
@@ -0,0 +1,80 @@
+<template>
+    <div
+        class="row justify-right items-center"
+        id="csc-conf-participants-cont"
+    >
+        <csc-conference-local-participant
+            ref="localParticipant"
+            :local-participant="localParticipant"
+            :local-media-stream="localMediaStream"
+            :is-microphone-enabled="isMicrophoneEnabled"
+            :is-camera-enabled="isCameraEnabled"
+            :is-screen-enabled="isScreenEnabled"
+        />
+        <div
+            id="csc-conf-remote-participants-cont"
+            v-for="participantId in participantsList" :key="participantId">
+            <csc-conference-remote-participant
+                :participant-id="participantId"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+    import {QCard, QCardMedia, QCardTitle} from 'quasar-framework'
+    import {mapGetters} from 'vuex'
+    import CscMedia from "../../CscMedia";
+    import CscConferenceRemoteParticipant from './CscConferenceRemoteParticipant'
+    import CscConferenceLocalParticipant from './CscConferenceLocalParticipant'
+
+    export default {
+        name: 'csc-conference-participants',
+        components: {
+            QCard,
+            QCardMedia,
+            QCardTitle,
+            CscMedia,
+            CscConferenceRemoteParticipant,
+            CscConferenceLocalParticipant
+        },
+        computed: {
+            ...mapGetters('conference', [
+                'participantsList',
+                'localParticipant',
+                'localMediaStream',
+                'isMicrophoneEnabled',
+                'isCameraEnabled',
+                'isScreenEnabled'
+            ])
+        },
+        mounted() {
+            this.assignLocalMediaStream(this.localMediaStream);
+        },
+        methods: {
+            assignLocalMediaStream(stream) {
+                if(this.$refs.localParticipant) {
+                    this.$refs.localParticipant.assignStream(stream);
+                }
+            }
+        },
+        watch: {
+            localMediaStream(stream) {
+                this.assignLocalMediaStream(stream);
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    @import '../../../themes/app.common.styl'
+    #csc-conf-participants-cont
+        float right
+        padding 10px
+        display inline-block
+        height calc(100vh - 150px)
+        overflow hidden
+
+    #csc-conf-remote-participants-cont
+        overflow scroll
+</style>
