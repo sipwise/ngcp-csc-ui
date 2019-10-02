8a48e2486c3cb3579b8a8ac361d1735b029bfa91
Carlo Venusino <cvenusino@sipwise.com>
2019-10-02 16:43:11 +0200

    TT#44287 Conferencing - display the list of participants
    - list of participants as column (subtask #67600)
    - participant displayName added (subtask #67605)
    - participants list scrollable (subtask #67601)
    - self participant item on top of the list (subtask #67602)
    - self participant fixed position on top of the list (subtask #67603)
    - placeholder icon if video not shared by participant (subtask #67309)
    - show/hear participants (subtask #67604 in progress )
    
    Change-Id: I44836b67767d243554e674e76486681865931f84

---
+++ src/components/pages/Conference/CscConferenceParticipants.vue
@@ -0,0 +1,88 @@
+<template>
+    <div
+        class="row justify-right items-center"
+        id="csc-conf-participants-cont"
+    >
+      <q-card
+        class="csc-conf-participants-item-cont"
+      >
+        <q-card-media
+          class="csc-avatar-cont"
+        >
+          <img src="/statics/avatar.png">
+        </q-card-media>
+        <q-card-title
+          class="csc-conf-participants-item-title"
+        >
+          {{localParticipant.displayName}}
+        </q-card-title>
+      </q-card>
+      <div
+        id="csc-conf-remote-participants-cont"
+        v-for="participantId in participantsList" :key="participantId">
+        <csc-conference-participant
+            :participant-id="participantId"
+        />
+    </div>
+    </div>
+</template>
+
+<script>
+    import { QCard, QCardMedia, QCardTitle } from 'quasar-framework'
+    import { mapGetters } from 'vuex'
+    import CscMedia from "../../CscMedia";
+    import CscConferenceParticipant from './CscConferenceParticipant'
+    export default {
+        name: 'csc-conference-participants',
+        components: {
+          QCard,
+          QCardMedia,
+          QCardTitle,
+          CscMedia,
+          CscConferenceParticipant,
+        },
+        computed: {
+          ...mapGetters('conference', [
+            'localParticipant',
+            'participantsList'
+          ])
+        }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    @import '../../../themes/app.common.styl'
+    #csc-conf-participants-cont
+      float right
+      padding 10px
+      display inline-block
+      height calc(100vh - 150px)
+      overflow hidden
+    #csc-conf-remote-participants-cont
+      overflow scroll
+    .csc-conf-participants-item-cont
+      margin-bottom 20px
+      width 115px
+      height 115px
+      background white
+    .csc-conf-participants-item-title
+      position relative
+      background white
+      bottom 24px
+      text-align center
+      padding 0
+      background rgba(0, 0, 0, 0.5)
+      .q-card-title
+        color $primary
+        font-size 12px
+        line-height 24px
+    .csc-avatar-cont
+      height 100%
+      top 6px
+    .csc-media-cont
+      height 100% !important
+      width 100% !important
+      video
+        height: 100%;
+
+</style>
