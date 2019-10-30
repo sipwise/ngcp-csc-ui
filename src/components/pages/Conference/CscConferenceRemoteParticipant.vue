8cf6234e6360662c2bded86b6f1d46fb0324ce13
Carlo Venusino <cvenusino@sipwise.com>
2019-10-30 14:35:20 +0100

    TT#47718 Select participant in main content area
    - selected ParticipantItem's video in fullscreen (#67702)
    - placeholder icon for none existing video of the current selected participant (#67706)
    - current selected ParticipantItem's name as title (#67703)
    - local participant selected by default (#67704)
    
    Change-Id: I6e2e47609adc407363be0c1636ff0ae8bd30ad38

--- src/components/pages/Conference/CscConferenceRemoteParticipant.vue
+++ src/components/pages/Conference/CscConferenceRemoteParticipant.vue
@@ -2,14 +2,12 @@
     <q-card
         class="csc-conf-participant-cont"
     >
-        <q-card-media
-            class="csc-avatar-cont"
-            v-show="!hasRemoteVideo"
+        <q-icon
+            name="person"
+            class="csc-conf-avatar"
+            v-if="!hasRemoteVideo"
         >
-            <img
-                src="statics/avatar.png"
-            />
-        </q-card-media>
+        </q-icon>
         <csc-media
             v-show="hasRemoteVideo"
             class="csc-media-cont"
@@ -26,16 +24,24 @@
 </template>
 
 <script>
-    import {QCard, QCardMedia, QCardTitle} from 'quasar-framework'
+    import { QIcon, QCard, QCardMedia, QCardTitle } from 'quasar-framework'
     import CscMedia from "../../CscMedia";
-
+    import {
+        mapGetters
+    } from 'vuex'
     export default {
         name: 'csc-conference-remote-participant',
         components: {
+            QIcon,
             QCard,
             QCardMedia,
             QCardTitle,
             CscMedia
+        },
+        data: function () {
+            return {
+                localMediaStream : null
+            }
         },
         props: [
             'remoteParticipant',
@@ -43,13 +49,23 @@
             'remoteMediaStreams',
             'hasRemoteVideo',
         ],
+        computed: {
+            ...mapGetters('conference', [
+                'selectedParticipant'
+            ])
+        },
         mounted() {
-            this.assignStream();
+            this.assignStream(this);
         },
         methods: {
-            assignStream() {
-                if (this.$refs.cscMedia && this.remoteMediaStreams[this.remoteParticipant.id] === this.remoteParticipant.id) {
-                    this.$refs.cscMedia.assignStream(this.remoteMediaStream(this.remoteParticipant.id));
+            assignStream: (scope) => {
+                if (scope.$refs.cscMedia && scope.remoteMediaStreams[scope.remoteParticipant.id] === scope.remoteParticipant.id) {
+                    scope.localMediaStream = scope.remoteMediaStream(scope.remoteParticipant.id);
+                    scope.$refs.cscMedia.assignStream(scope.localMediaStream);
+                    if(scope.selectedParticipant == scope.remoteParticipant.id){
+                        scope.$store.commit('conference/setSelectedParticipant', 'local'); // TODO improve (workaround to reset the mediaStream)
+                        scope.$store.commit('conference/setSelectedParticipant', scope.remoteParticipant.id);
+                    }
                 }
                 else if (this.$refs.cscMedia) {
                     this.$refs.cscMedia.reset();
@@ -58,7 +74,7 @@
         },
         watch: {
             remoteMediaStreams() {
-                this.assignStream();
+                this.assignStream(this);
             }
         }
     }
