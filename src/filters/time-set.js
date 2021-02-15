f7c6f9745bf79459567612bcf8b22d1c5a99d796
Sergii Leonenko <sleonenko@sipwise.com>
2021-02-15 21:43:21 +0200

    TT#109763 Fix OfficeHours in the new CallForwarding implementation
    AC:
    Can see "Office Hours"-Condition working properly according to the Kamailio time periods
    Can save and see the same periods after loaded the data again
    
    In addition:
    * added logic to automatic detecion and seting "Same Time..." checkbox state based on the time ranges data in the timeset instead of timeset name
    * added time ranges validation
    * reproduced notification about invalid timeset which was in the old UI
    * fixed popup layout to support long list of time ranges
    
    Change-Id: I664d7871d1d212e2b5df362b28be48b9ee5c790e

--- src/filters/time-set.js
+++ src/filters/time-set.js
@@ -1,17 +1,8 @@
 
 import { i18n } from 'boot/i18n'
+import { getKamailioRangeElements } from 'src/helpers/kamailio-timesets-converter'
 
 export const DAY_MAP = [2, 3, 4, 5, 6, 7, 1]
-export const DAY_NAME_MAP = [
-	i18n.t('Monday'),
-	i18n.t('Tuesday'),
-	i18n.t('Wednesday'),
-	i18n.t('Thursday'),
-	i18n.t('Friday'),
-	i18n.t('Saturday'),
-	i18n.t('Sunday')
-]
-
 export const DEFAULT_WEEKDAYS = [
 	DAY_MAP[0],
 	DAY_MAP[1],
@@ -19,6 +10,29 @@ export const DEFAULT_WEEKDAYS = [
 	DAY_MAP[3],
 	DAY_MAP[4]
 ]
+
+export function getDayNameByNumber (dayNumber, isShortName = false) {
+	const daysNamesMap = [
+		i18n.t('Monday'),
+		i18n.t('Tuesday'),
+		i18n.t('Wednesday'),
+		i18n.t('Thursday'),
+		i18n.t('Friday'),
+		i18n.t('Saturday'),
+		i18n.t('Sunday')
+	]
+	// NOTE: in some languages the short days names may be not just first two letters of the full day's name
+	const daysShortNamesMap = [
+		i18n.t('Mo'),
+		i18n.t('Tu'),
+		i18n.t('We'),
+		i18n.t('Th'),
+		i18n.t('Fr'),
+		i18n.t('Sa'),
+		i18n.t('Su')
+	]
+	return isShortName ? daysShortNamesMap[dayNumber] : daysNamesMap[dayNumber]
+}
 
 export function timeSetDateExact (times) {
 	return times[0].year + '/' + times[0].month + '/' + times[0].mday
@@ -37,43 +51,25 @@ export function timeSetWeekdays (times) {
 		return DAY_MAP.indexOf(parseInt(time.wday))
 	})
 	mappedWeekdays.sort()
-	let weekdays = ''
-	mappedWeekdays.forEach((weekday, index) => {
-		if (index > 0) {
-			weekdays = weekdays + ', '
-		}
-		weekdays = weekdays + DAY_NAME_MAP[weekday]
-	})
-	return weekdays
+	const weekdaysStr = mappedWeekdays.map(weekday => getDayNameByNumber(weekday)).join(', ')
+	return weekdaysStr
 }
 
 export function timeSetOfficeHoursSameTime (times) {
 	const weekdays = new Set()
-	let weekdaysStr = ''
-	times.forEach((time) => {
-		weekdays.add(parseInt(time.wday))
+	times.forEach(kamailioTimeRange => {
+		const wdayRange = getKamailioRangeElements(kamailioTimeRange.wday)[0]
+		for (let day = wdayRange.from; day <= wdayRange.to; day++) {
+			// To have Monday the first day of a week we are mapping days to change order
+			weekdays.add(DAY_MAP.indexOf(day))
+		}
 	})
 	const weekdaysSorted = Array.from(weekdays)
-	weekdaysSorted.sort((a, b) => {
-		if (a === 1) {
-			return 1
-		} else if (a > b) {
-			return 1
-		} else if (a < b) {
-			return -1
-		} else {
-			return 0
-		}
-	})
-	weekdaysSorted.forEach((weekday, index) => {
-		if (index > 0) {
-			weekdaysStr += ', '
-		}
-		weekdaysStr += DAY_NAME_MAP[DAY_MAP.indexOf(weekday)]
-	})
+	weekdaysSorted.sort()
+	const weekdaysStr = weekdaysSorted.map(weekday => getDayNameByNumber(weekday)).join(', ')
 	return weekdaysStr
 }
 
 export function timeSetTimes () {
-
+	return '...'
 }
