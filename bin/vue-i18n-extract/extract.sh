91b157efaf489f88b6bfc84bd88341375908ad6b
Sergii Leonenko <sleonenko@sipwise.com>
2021-06-25 17:55:04 +0300

    TT#127852 i18n - Extraction tool must extract to english language file only
    Change-Id: I3b1828847310234a15e3d5db4826630b76941207
    (cherry picked from commit f5c1f457c4365508e814818c34393b3535506ba5)

--- bin/vue-i18n-extract/extract.sh
+++ bin/vue-i18n-extract/extract.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ "$1" == "report" ]; then
-    OTHER_PARAMS=()
+    OTHER_PARAMS=(--main-language-to-sync-keys en -l './src/i18n/*.json')
 else
-    OTHER_PARAMS=(--add --json-sort-keys --json-indent-characters "4,space" --no-detailed-report)
+    OTHER_PARAMS=(--add --json-sort-keys --json-indent-characters "4,space" --no-detailed-report -l './src/i18n/en.json')
 fi
 
-node ./bin/vue-i18n-extract/vue-i18n-extract.js report -v './src/**/*.?(js|vue)' -l './src/i18n/*.json' \
-    --no-dot-notation --add-value-for-languages en --main-language-to-sync-keys en "${OTHER_PARAMS[@]}"
+node ./bin/vue-i18n-extract/vue-i18n-extract.js report -v './src/**/*.?(js|vue)' \
+    --no-dot-notation --add-value-for-languages en "${OTHER_PARAMS[@]}"
