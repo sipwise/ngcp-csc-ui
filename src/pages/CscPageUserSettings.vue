30d02857b13c92b3b9de54c78cc94175e050caff
Sergii Leonenko <sleonenko@sipwise.com>
2021-08-05 23:40:36 +0200

    TT#134651 SIPAuth - As Subscriber/PBXAttendant, I want to change my SIP-Password, in order to authenticate my SIP-Device against NGCP
    Change-Id: If81c45a5994d72c0abe2455f880420b9e95cc45f

--- src/pages/CscPageUserSettings.vue
+++ src/pages/CscPageUserSettings.vue
@@ -7,11 +7,51 @@
             class="col col-xs-12 col-md-6"
         >
             <csc-change-password
+                class="q-mb-md"
                 :loading="isPasswordChanging"
                 :error="changePasswordError"
                 :subscriber="getSubscriber"
                 @change="changePassword"
             />
+            <csc-change-password-embedded
+                ref="changeSipPasswordSection"
+                class="q-mb-md"
+                :btn-label="$t('Change SIP Password')"
+                :password-label="$t('New SIP Password')"
+                :password-confirm-label="$t('New SIP Password confirm')"
+                :loading="processingChangeSIPPassword"
+                @change="requestSIPPasswordChange"
+            />
+            <q-input
+                :value="currentSIPURI"
+                :label="$t('SIP URI')"
+                readonly
+            >
+                <template v-slot:append>
+                    <q-btn
+                        icon="content_copy"
+                        color="primary"
+                        flat
+                        dense
+                        @click="copy2clipboard(currentSIPURI)"
+                    />
+                </template>
+            </q-input>
+            <csc-input-password
+                :value="currentSIPPassword"
+                :label="$t('SIP Password')"
+                readonly
+            >
+                <template v-slot:append>
+                    <q-btn
+                        icon="content_copy"
+                        color="primary"
+                        flat
+                        dense
+                        @click="copy2clipboard(currentSIPPassword)"
+                    />
+                </template>
+            </csc-input-password>
         </div>
     </csc-page>
 </template>
@@ -31,10 +71,16 @@ import {
 } from 'vuex'
 import CscPage from 'components/CscPage'
 import CscChangePassword from 'components/pages/UserSettings/CscChangePassword'
+import CscChangePasswordEmbedded from 'components/pages/UserSettings/CscChangePasswordEmbeded'
+import { mapWaitingActions, mapWaitingGetters } from 'vue-wait'
+import { copyToClipboard } from 'quasar'
+import CscInputPassword from 'components/form/CscInputPassword'
 
 export default {
     name: 'CscPageUserSettings',
     components: {
+        CscInputPassword,
+        CscChangePasswordEmbedded,
         CscChangePassword,
         CscPage
     },
@@ -50,7 +96,17 @@ export default {
         ...mapGetters('user', [
             'getSubscriber',
             'isPasswordChanging'
-        ])
+        ]),
+        ...mapWaitingGetters({
+            processingChangeSIPPassword: 'processing-changeSIPPassword'
+        }),
+        currentSIPPassword () {
+            return this.getSubscriber?.password || ''
+        },
+        currentSIPURI () {
+            const subscriberData = this.getSubscriber
+            return subscriberData?.username + '@' + subscriberData?.domain
+        }
     },
     watch: {
         changePasswordState (state) {
@@ -64,7 +120,29 @@ export default {
     methods: {
         ...mapActions('user', [
             'changePassword'
-        ])
+        ]),
+        ...mapWaitingActions('user', {
+            changeSIPPassword: 'processing-changeSIPPassword'
+        }),
+        async requestSIPPasswordChange (newPassword) {
+            try {
+                await this.changeSIPPassword(newPassword)
+                showToast(this.$t('Your SIP password has been changed successfully'))
+                this.$refs.changeSipPasswordSection.cancel()
+            } catch (error) {
+                showGlobalError(error?.message)
+            }
+        },
+        copy2clipboard (copyData) {
+            copyToClipboard(copyData)
+                .then(() => {
+                    showToast(this.$t('Data is in the clipboard'))
+                })
+                .catch(() => {
+                    console.error(copyData)
+                    showGlobalError(this.$t('Unable to copy data to clipboard'))
+                })
+        }
     }
 }
 </script>
