8975d7869ece258233e15ccee6f517e2cedba30c
Hugo Zigha <hugo.zigha@al-enterprise.com>
2023-11-30 10:31:14 +0100

    MT#58893 Fix build failures for missing jest module
    The jest module cannot be found after the update due
    to a wrong transformation:
    
    07:46:39 yarn run v1.22.19
    07:46:39 $ jest --updateSnapshot
    07:46:39 ● Validation Error:
    07:46:39
    07:46:39   Module vue-jest in the transform option was not found.
    07:46:39          <rootDir> is: /tmp/code
    07:46:39
    07:46:39   Configuration Documentation:
    07:46:39   https://jestjs.io/docs/configuration
    
    This commit fixes the transformation and points to the new
    '@vue/vue3-jest'
    
    Change-Id: If781a23155352d0e3275c064bbca92d17076dbad

--- test/jest/jest.setup.js
+++ test/jest/jest.setup.js
@@ -5,8 +5,6 @@ jest.setTimeout(1000)
 // jest speedup when errors are part of the game
 // Error.stackTraceLimit = 0
 
-global.Promise = require('promise')
-
 /*
 import chai from 'chai'
 // Make sure chai and jasmine ".not" play nice together
