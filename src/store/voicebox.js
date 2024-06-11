aff80a1e1f5b17b24ace2d410cb83f6a5dc014b8
Nouhaila Idrissi Zouggari <nouhaila.idrissi-zouggari@al-enterprise.com>
2024-06-11 13:34:27 +0200

    MT#60292 Selection for announcements missing : temp & greet
    Added two announcements: Temp/Greet Greeting in the voicebox section
    
    Change-Id: I6026136eace78ef9d5bbdb27d99ad102ab0ee40d

--- src/store/voicebox.js
+++ src/store/voicebox.js
@@ -68,7 +68,25 @@ export default {
         unavailableGreetingDeletionState: RequestState.initiated,
         unavailableGreetingDeletionError: null,
 
-        uploadCancelActions: {}
+        uploadCancelActions: {},
+
+        tempGreetingId: null,
+        tempGreetingUrl: null,
+        tempGreetingLoadState: RequestState.initiated,
+        tempGreetingUploadState: RequestState.initiated,
+        tempGreetingUploadError: null,
+        tempGreetingUploadProgress: 0,
+        tempGreetingDeletionState: RequestState.initiated,
+        tempGreetingDeletionError: null,
+
+        greetGreetingId: null,
+        greetGreetingUrl: null,
+        greetGreetingLoadState: RequestState.initiated,
+        greetGreetingUploadState: RequestState.initiated,
+        greetGreetingUploadError: null,
+        greetGreetingUploadProgress: 0,
+        greetGreetingDeletionState: RequestState.initiated,
+        greetGreetingDeletionError: null
     },
     getters: {
         subscriberId (state, getters, rootState, rootGetters) {
@@ -144,6 +162,34 @@ export default {
         },
         unavailableGreetingDeleting (state) {
             return state.unavailableGreetingDeletionState === RequestState.requesting
+        },
+        tempGreetingUploading (state) {
+            return state.tempGreetingUploadState === RequestState.requesting
+        },
+        tempGreetingFileLoaded (state) {
+            return state.tempGreetingLoadState === RequestState.succeeded
+        },
+        tempGreetingLabel (state) {
+            return state.tempGreetingId
+                ? i18n.global.tc('Custom sound')
+                : i18n.global.tc('Default sound')
+        },
+        tempGreetingDeleting (state) {
+            return state.tempGreetingDeletionState === RequestState.requesting
+        },
+        greetGreetingUploading (state) {
+            return state.greetGreetingUploadState === RequestState.requesting
+        },
+        greetGreetingFileLoaded (state) {
+            return state.greetGreetingLoadState === RequestState.succeeded
+        },
+        greetGreetingLabel (state) {
+            return state.greetGreetingId
+                ? i18n.global.tc('Custom sound')
+                : i18n.global.tc('Default sound')
+        },
+        greetGreetingDeleting (state) {
+            return state.greetGreetingDeletionState === RequestState.requesting
         }
     },
     mutations: {
@@ -163,6 +209,12 @@ export default {
             }
             if (_.has(res, 'unavailableGreetingId') && res.unavailableGreetingId !== null) {
                 state.unavailableGreetingId = res.unavailableGreetingId
+            }
+            if (_.has(res, 'tempGreetingId') && res.tempGreetingId !== null) {
+                state.tempGreetingId = res.tempGreetingId
+            }
+            if (_.has(res, 'greetGreetingId') && res.greetGreetingId !== null) {
+                state.greetGreetingId = res.greetGreetingId
             }
         },
 
@@ -319,6 +371,96 @@ export default {
         },
         updateUploadCancelActions (state, field, value) {
             state.uploadCancelActions[field] = value
+        },
+        tempGreetingUploadProgress (state, progress) {
+            state.tempGreetingUploadProgress = progress
+        },
+        tempGreetingUploadRequesting (state) {
+            state.tempGreetingUploadState = RequestState.requesting
+            state.tempGreetingUploadError = null
+            state.tempGreetingUploadProgress = 0
+        },
+        tempGreetingUploadSucceeded (state, id) {
+            state.tempGreetingUploadState = RequestState.succeeded
+            state.tempGreetingUploadError = null
+            state.tempGreetingId = id
+        },
+        tempGreetingUploadFailed (state, error) {
+            state.tempGreetingUploadState = RequestState.failed
+            state.tempGreetingUploadError = error
+            state.tempGreetingUploadProgress = 0
+        },
+        tempGreetingPlayRequesting (state) {
+            state.tempGreetingLoadState = RequestState.requesting
+            state.tempGreetingUrl = null
+        },
+        tempGreetingPlaySucceeded (state, url) {
+            state.tempGreetingLoadState = RequestState.succeeded
+            state.tempGreetingUrl = url
+        },
+        tempGreetingPlayFailed (state) {
+            state.tempGreetingLoadState = RequestState.failed
+            state.tempGreetingUrl = null
+        },
+        tempGreetingDeletionRequesting (state) {
+            state.tempGreetingDeletionState = RequestState.requesting
+            state.tempGreetingDeletionError = null
+        },
+        tempGreetingDeletionFailed (state, error) {
+            state.tempGreetingDeletionState = RequestState.failed
+            state.tempGreetingDeletionError = error
+        },
+        tempGreetingDeletionSucceeded (state) {
+            state.tempGreetingDeletionState = RequestState.succeeded
+            state.tempGreetingDeletionError = null
+            state.tempGreetingId = null
+            state.tempGreetingLoadState = RequestState.initiated
+            state.tempGreetingUrl = null
+        },
+        greetGreetingUploadRequesting (state) {
+            state.greetGreetingUploadState = RequestState.requesting
+            state.greetGreetingUploadError = null
+            state.greetGreetingUploadProgress = 0
+        },
+        greetGreetingUploadSucceeded (state, id) {
+            state.greetGreetingUploadState = RequestState.succeeded
+            state.greetGreetingUploadError = null
+            state.greetGreetingId = id
+        },
+        greetGreetingUploadProgress (state, progress) {
+            state.greetGreetingUploadProgress = progress
+        },
+        greetGreetingUploadFailed (state, error) {
+            state.greetGreetingUploadState = RequestState.failed
+            state.greetGreetingUploadError = error
+            state.greetGreetingUploadProgress = 0
+        },
+        greetGreetingPlayRequesting (state) {
+            state.greetGreetingLoadState = RequestState.requesting
+            state.greetGreetingUrl = null
+        },
+        greetGreetingPlaySucceeded (state, url) {
+            state.greetGreetingLoadState = RequestState.succeeded
+            state.greetGreetingUrl = url
+        },
+        greetGreetingPlayFailed (state) {
+            state.tgreetGreetingLoadState = RequestState.failed
+            state.greetGreetingUrl = null
+        },
+        greetGreetingDeletionRequesting (state) {
+            state.greetGreetingDeletionState = RequestState.requesting
+            state.greetGreetingDeletionError = null
+        },
+        greetGreetingDeletionSucceeded (state) {
+            state.greetGreetingDeletionState = RequestState.succeeded
+            state.greetGreetingDeletionError = null
+            state.greetGreetingId = null
+            state.greetGreetingLoadState = RequestState.initiated
+            state.greetGreetingUrl = null
+        },
+        greetGreetingDeletionFailed (state, error) {
+            state.greetGreetingDeletionState = RequestState.failed
+            state.greetGreetingDeletionError = error
         }
     },
     actions: {
@@ -334,12 +476,22 @@ export default {
                 getVoiceboxGreetingByType({
                     id: subscriberId,
                     type: 'unavail'
+                }),
+                getVoiceboxGreetingByType({
+                    id: subscriberId,
+                    type: 'temp'
+                }),
+                getVoiceboxGreetingByType({
+                    id: subscriberId,
+                    type: 'greet'
                 })
             ])
             context.commit('settingsSucceeded', {
                 settings: res[0],
                 busyGreetingId: _.get(res, '1.items.0.id', null),
-                unavailableGreetingId: _.get(res, '2.items.0.id', null)
+                unavailableGreetingId: _.get(res, '2.items.0.id', null),
+                tempGreetingId: _.get(res, '3.items.0.id', null),
+                greetGreetingId: _.get(res, '4.items.0.id', null)
             })
         },
         async updateAction (context, options) {
@@ -494,6 +646,54 @@ export default {
             await context.dispatch('greetingDelete', {
                 type: 'unavailable',
                 id: context.state.unavailableGreetingId
+            })
+        },
+        async tempGreetingUpload (context, options) {
+            await context.dispatch('greetingUpload', {
+                type: 'temp',
+                greeting: 'temp',
+                file: options.file,
+                subscriberId: options.subscriberId
+            })
+        },
+        async tempGreetingUploadAbort (context) {
+            await context.dispatch('greetingUploadAbort', 'temp')
+        },
+        async tempGreetingPlay (context, format) {
+            await context.dispatch('greetingPlay', {
+                type: 'temp',
+                id: context.state.tempGreetingId,
+                format: format
+            })
+        },
+        async tempGreetingDelete (context) {
+            await context.dispatch('greetingDelete', {
+                type: 'temp',
+                id: context.state.tempGreetingId
+            })
+        },
+        async greetGreetingUpload (context, options) {
+            await context.dispatch('greetingUpload', {
+                type: 'greet',
+                greeting: 'greet',
+                file: options.file,
+                subscriberId: options.subscriberId
+            })
+        },
+        async greetGreetingUploadAbort (context) {
+            await context.dispatch('greetingUploadAbort', 'greet')
+        },
+        async greetGreetingPlay (context, format) {
+            await context.dispatch('greetingPlay', {
+                type: 'greet',
+                id: context.state.greetGreetingId,
+                format: format
+            })
+        },
+        async greetGreetingDelete (context) {
+            await context.dispatch('greetingDelete', {
+                type: 'greet',
+                id: context.state.greetGreetingId
             })
         }
     }
