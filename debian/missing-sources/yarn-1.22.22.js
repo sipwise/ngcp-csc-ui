741b074edb0b08fcbf4cf630191b52c804300e10
Debora Crescenzo <dcrescenzo@sipwise.com>
2024-11-14 15:43:20 +0000

    MT#61534 Improve bundling
    Set 1.22.22 as default yarn version for the repo
    
    Change-Id: Iea323c46ff0f44cab81be3a1fa5748d93f983b5b
    (cherry picked from commit 24cda522b631fed7ecd75665003e94d334b00efb)
    (cherry picked from commit 60f9f71d931221a2ccd1fee1df59101fec2f86bc)

---
+++ debian/missing-sources/yarn-1.22.22.js
@@ -0,0 +1,8 @@
+/*
+The file .yarn/releases/yarn-1.22.22.js is a copy of
+https://github.com/yarnpkg/yarn/releases/download/v1.22.22/yarn-1.22.22.js
+
+Its source code is available at
+https://github.com/yarnpkg/yarn/archive/refs/tags/v1.22.22.tar.gz
+(and a copy of this is available at https://deb.sipwise.com/files/yarn_v1.22.22.tar.gz )
+*/
