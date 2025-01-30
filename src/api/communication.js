e350bb986041c6b0f401639e38771d98ce17540c
Debora Crescenzo <dcrescenzo@sipwise.com>
2025-01-30 14:39:13 +0000

    MT#62025 Send Fax 422 error bugfix
    /api/faxes endpoint was returning a 422 error
    Uprocessable content. This commit
    amends the payload to match what is in the
    APIv1 documentation (old docs)
    
    Change-Id: Ib8d9c0ec41e1e1a18bbe503db711b55e5f66e050
    (cherry picked from commit 47d46279803aabc7987e55b30bc6786ece876b47)

--- src/api/communication.js
+++ src/api/communication.js
@@ -1,17 +1,12 @@
-import _ from 'lodash'
 import { post } from 'src/api/common'
 
 export async function createFax (options) {
-    const formData = new FormData()
-    const fields = _.clone(options)
-    delete fields.faxfile
-    const json = JSON.stringify(fields)
-    formData.append('json', json)
-    if (options.faxfile) {
-        formData.append('faxfile', options.faxfile)
+    if (options.faxfile === null) {
+        delete options.faxfile
     }
+
     return await post({
         resource: 'faxes',
-        body: formData
+        body: options
     })
 }
