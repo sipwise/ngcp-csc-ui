1f523513e158d6f822475823ac54049dc6663aa6
Debora Crescenzo <dcrescenzo@sipwise.com>
2025-02-05 13:10:41 +0000

    MT#62025 Add multipart/form-data to api/faxes req
    Revert original code and just add missing
    header.
    
    Change-Id: If109b4ecc3810aad7cae5b8dc1b0dd87a7d6e325
    (cherry picked from commit 3daeb267bb1c8838d6d7aaa06478c9ae067ea4f9)

--- src/api/communication.js
+++ src/api/communication.js
@@ -1,12 +1,18 @@
+import _ from 'lodash'
 import { post } from 'src/api/common'
 
 export async function createFax (options) {
-    if (options.faxfile === null) {
-        delete options.faxfile
+    const formData = new FormData()
+    const fields = _.clone(options)
+    delete fields.faxfile
+    const json = JSON.stringify(fields)
+    formData.append('json', json)
+    if (options.faxfile) {
+        formData.append('faxfile', options.faxfile)
     }
-
     return await post({
         resource: 'faxes',
-        body: options
+        headers: { 'Content-Type': 'multipart/form-data' },
+        body: formData
     })
 }
