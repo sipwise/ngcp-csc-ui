325e32c5a74c6f8d48317861595e42e607424eb0
nidrissi-zouggari <nouhaila.idrissi-zouggari@al-enterprise.com>
2026-02-02 14:53:27 +0100

    MT#64240 Fix call page input bug
    - Keep callNumberInput getter returning full input for display purposes
    - Use callNumberNormalized getter to strip domain before making calls
    - Add callNumberNormalized to component's mapped getters
    - Update startCall validation to use normalized number
    
    Change-Id: I4172a4f426bf7f827e3ff717a901b8a64f4264d1
    (cherry picked from commit afd3e09d551c6df4fc1d5aa0f8aeeecd825937aa)
    (cherry picked from commit 39dded7bd6bda30aab3da51cab52e6bc12b1e4e1)

--- src/store/call/actions.js
+++ src/store/call/actions.js
@@ -27,10 +27,7 @@ let errorVisibilityTimer = null
 
 export default {
     async start (context, localMedia) {
-        const number = context.getters.callNumberInput.replaceAll('(', '')
-            .replaceAll(')', '')
-            .replaceAll(' ', '')
-            .replaceAll('-', '')
+        const number = context.getters.callNumberNormalized.replaceAll('(', '').replaceAll(')', '').replaceAll(' ', '').replaceAll('-', '')
         context.commit('startCalling', number)
         const isStarted = await callStart({
             number,
