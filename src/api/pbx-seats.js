377ddd1d7876a672d76420e01c2877815e98e468
nidrissi-zouggari <nouhaila.idrissi-zouggari@al-enterprise.com>
2026-04-14 14:31:52 +0200

    MT#64678 Remove unnecessary preferences from PBX Seats creation form and actions menu
    - Removed "Hide number within own PBX" from PBX Seat creation form
    - Removed "Hide number within own PBX" from PBX Seats actions menu
    - Removed "Music on hold" from PBX Seats actions menu
    
    These preferences are now only configurable in the PBX Seat details to avoid duplication and simplify the UI.
    
    Change-Id: I8c9c8967bf4039f02b5848dea33285d04b0451c1
    (cherry picked from commit 2971827b0acd7c790fbabc5e8b7b96ef0c634d79)

--- src/api/pbx-seats.js
+++ src/api/pbx-seats.js
@@ -154,7 +154,6 @@ export async function createSeat (seat) {
             }, { forceCli: seat.forceCli })
         }).then(($subscriberId) => {
             subscriberId = $subscriberId
-            setSeatIntraPbx(subscriberId, seat.clirIntrapbx)
             if (seat.soundSet !== null && seat.soundSet !== undefined) {
                 return getSoundSet(seat.soundSet)
             }
