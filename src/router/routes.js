6a5d4cab3e115dcbe071b2828543e539d8a8cd91
Debora Crescenzo <dcrescenzo@sipwise.com>
2026-09-23 09:20:33 +0100

    MT#66066 Fix Call Recordings license typo
    Change-Id: I39d71b40b746ea4f490e3936fc7c4f2cc709796e
    (cherry picked from commit b1207bde14b719a5dc357c2e9c78b4bd8b147e50)

--- src/router/routes.js
+++ src/router/routes.js
@@ -205,7 +205,7 @@ const routes = [
                         return i18n.global.t('Call recordings')
                     },
                     profileAttribute: PROFILE_ATTRIBUTE_MAP.recordings,
-                    licenses: [LICENSES.recordings],
+                    licenses: [LICENSES.call_recording],
                     allowCE: true
                 }
             },
